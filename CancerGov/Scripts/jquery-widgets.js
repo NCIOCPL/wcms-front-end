2019e12781322ee948120a9e38df735d90229800
Mark Cramer <markecramer@users.noreply.github.com>
2011-11-29 17:21:35 +0000

    added javascript for science snapshots in cancer bulletin

--- CancerGov/Scripts/jquery-widgets.js
+++ CancerGov/Scripts/jquery-widgets.js
@@ -857,3 +857,28 @@ function onYouTubePlayerReady(playerId) {
 
 
 /* END 508 Video Player */
+
+
+/* BEGIN Cancer Bulletin Science Shots */
+var state = 'none';
+function showhide(layer_ref) {
+if (state == 'block') { 
+state = 'none'; 
+} 
+else { 
+state = 'block'; 
+} 
+if (document.all) { //IS IE 4 or 5 (or 6 beta) 
+eval( "document.all." + layer_ref + ".style.display = state"); 
+} 
+if (document.layers) { //IS NETSCAPE 4 or below 
+document.layers[layer_ref].display = state; 
+} 
+if (document.getElementById) {
+if (!document.all) { 
+hza = document.getElementById(layer_ref); 
+hza.style.display = state; 
+}
+}
+}
+/* END Cancer Bulletin Science Shots */
