26e266e84d48bbfbd98d038b603c74f13182527a
schultznr <webcommauto@users.noreply.github.com>
2012-02-14 18:52:06 +0000

    removed debug prints

--- CancerGov-Mobile/Scripts/jump-links.js
+++ CancerGov-Mobile/Scripts/jump-links.js
@@ -8,17 +8,14 @@ var sibList;
 var kidList;
 while(parent != document){
 	if($(parent).hasClass('ui-collapsible-content') && $(parent).hasClass('ui-collapsible-content-collapsed')){
-		console.log(parent);
 		sibList = parent.parentNode.childNodes
 		for(var i = 0; i< sibList.length; i++){			
 		sib = sibList[i];
 			if($(sib).hasClass('ui-collapsible-heading')){
-			console.log(sib);
 			kidList = sib.childNodes;
 			for(var j = 0; j< kidList.length; j++){		
 				kid = kidList[i];
 					if($(kid).hasClass('ui-collapsible-heading-toggle')){
-						console.log("CLICK");
 						$(kid).click();
 					}
 				}
@@ -40,11 +37,8 @@ if(location.hash != ""){
 	$("html, body").animate({ scrollTop: $(location.hash).offset().top }, 1000);
 }
 $('.body-container').find('a[href*="#"]').bind('click', function(e){
-	console.log(this.baseURI);
-	console.log(document.baseURI);
 if (this.baseURI == document.baseURI){
 	var id = this.hash.substring(1,this.hash.length);
-	console.log(id);
 	if(id!=""){
 		gotoHash(id,$);
 	}
