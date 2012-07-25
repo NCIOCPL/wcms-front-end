90629d65d91cb0aeb982a30c42305b2f1490d39b
soldanji <webcommauto@users.noreply.github.com>
2012-07-25 19:54:50 +0000

    jQuery to add div around tables to better display them

--- CancerGov-Mobile/Scripts/ajax-linking.js
+++ CancerGov-Mobile/Scripts/ajax-linking.js
@@ -8,3 +8,8 @@
     $.mobile.ajaxEnabled = false;
 
 });
+
+// wrap a div with overflow: auto around all tables in the body
+$(document).ready(function() {
+	$('div#cgvMobileBody table').wrap('<div style="overflow: auto;"></div>');
+});
