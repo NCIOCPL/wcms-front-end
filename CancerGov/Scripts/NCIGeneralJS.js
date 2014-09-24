4796689367d4e49e200be91edc0afdc1fd1065e4
shahaj <webcommauto@users.noreply.github.com>
2014-09-24 19:54:18 +0000

    Updated enlarge images and table javascript call per changes from Volker.

--- CancerGov/Scripts/NCIGeneralJS.js
+++ CancerGov/Scripts/NCIGeneralJS.js
@@ -33,8 +33,7 @@ if($('.with-image').length == 0){
 $( "#cgvSiteBanner img, #cgvFooter img, .leftzone img, .news-slider-tabs img" ).attr( "nopin", "nopin" );
 
 /*code to enlarge an image or a table*/
-  $( ".expandable_container > img" ).supersizeme( { } );
-          $( "table" ).supersizeme( { } );
-		    
+  $( ".expandable-container" ).supersizeme( { } );
+       		    
 		  
 });
