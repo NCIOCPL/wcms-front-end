67454255d648b0a3e560649ba1cb4f24729a3760
burelbachwg <webcommauto@users.noreply.github.com>
2015-01-23 19:29:56 +0000

    [OCEPROJECT-2050, OCEPROJECT-2056] Added more extensibility to TOC-initialize.js (now works on .accordion h2, #nvcgRelatedResourcesArea h6, and #cgvCitationSl h6) and medium-down.scss (should now work on all accordions)

--- CancerGov/_src/Scripts/TOC-initialize.js
+++ CancerGov/_src/Scripts/TOC-initialize.js
@@ -4,24 +4,49 @@ var accordionize = function() {
 	/* determine window width */
 	var width = window.innerWidth || $(window).width();
 
-	var targets = '.accordion, #accordion';
+	var targets = {
+		//'selector' : 'header'
+		'.accordion' : 'h2',
+		'#nvcgRelatedResourcesArea' : 'h6',
+		'#cgvCitationSl' : 'h6'
+	};
+	var targetsSelector = Object.keys(targets).join(', ');
+	var targetsBuiltAccordion = [],
+	    targetsHeader = [],
+	    accordion;
+
+	for(var target in targets) {
+		if(targets.hasOwnProperty(target)) {
+			targetsBuiltAccordion.push(target + '.ui-accordion');
+			targetsHeader.push(target + ' ' + targets[target]);
+		}
+	}
+	var targetsBuiltAccordionSelector = targetsBuiltAccordion.join(', ');
+	var targetsHeaderSelector = targetsHeader.join(', ');
 
 	/* If the width is less than or equal to 1024px (small/medium screens)
 	 * AND if the accordion(s) isn't (aren't) already built */
-	if (width <= 1024 && $('.accordion.ui-accordion, #accordion.ui-accordion').length === 0) {
+	if (width <= 1024 && $(targetsBuiltAccordionSelector).length === 0) {
 		// verify that the accordion will build correctly
-		$('.accordion h2, #accordion h2').each(function() {
+		$('.accordion h2').each(function() {
 			if($(this).nextAll().length > 1) {
 				$(this).nextUntil('h2').wrapAll('<div class="clearfix"></div>');
 			}
 		});
 
-		// actually build the accordion
-		NCI.doAccordion(targets);
+		for(accordion in targets) {
+			if(targets.hasOwnProperty(accordion)) {
+				NCI.doAccordion(accordion, {'header': targets[accordion]});
+			}
+		}
 
 		/* else, the window must be large */
 	} else if(width >= 1025) {
-		NCI.undoAccordion(targets);
+		for(accordion in targets) {
+			if(targets.hasOwnProperty(accordion)) {
+				NCI.undoAccordion(accordion, {'header': targets[accordion]});
+			}
+		}
 	}
 };
 
