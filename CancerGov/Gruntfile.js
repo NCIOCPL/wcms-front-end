92f96de90dd485aa06467797dc0eb619fe5c5076
calamiasj <webcommauto@users.noreply.github.com>
2015-03-17 18:24:02 +0000

    New & Improved Build Process

--- CancerGov/Gruntfile.js
+++ CancerGov/Gruntfile.js
@@ -1,55 +1,140 @@
 module.exports = function(grunt) {
+
+    // Define Dirs
+    grunt.config('dirs', {
+        src: {
+            base:       "_src/",
+            pages:      "_src/PageTemplates/",
+            styles:     "_src/StyleSheets/",
+            scripts:    "_src/Scripts/",
+        },
+        dist:   {
+            base:       "_dist/",
+            pages:      "_dist/PageTemplates/",
+            styles:     "_dist/css/",
+            scripts:    "_dist/js/",
+        },
+    });
+
     // Project Config
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'expanded',
-                precision: 5
+    grunt.config('pkg', grunt.file.readJSON('package.json'));
+
+    // Environment?
+    grunt.config('env', grunt.option('env') || process.env.GRUNT_DEV || 'development');
+
+    // Load Plugins
+    /*****************************************
+     *  SVN Repo Updating
+     ****************************************/
+    grunt.loadNpmTasks('grunt-svn-fetch');
+    grunt.config('svn_fetch', {
+        options: {
+            'repository':   
+            "<%= pkg.repository.url %><%= pkg.repository.branchdir %><%= pkg.repository.devbranch %><%= pkg.repository.subpath %>",
+            //'path':   '<%= pkg.repository.path %><%= pkg.repository.branchdir %><%= pkg.repository.devbranch %><%= pkg.repository.subpath %>'
+        },
+        nvcg: {
+            map: {
+                "_src":"_src",
+            }
+        }
+    });
+
+    /*****************************************
+     *  SASS Preprocessing
+     ****************************************/
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.config('sass', {
+        options: {
+            sourceMap: true,
+            outputStyle: 'expanded',
+            precision: 5
+        },
+        dist: {
+            files: {
+                '<%= dirs.dist.styles %>nvcg.css': '<%= dirs.src.styles %>nvcg.scss'
+            }
+        }
+    });
+
+    /*****************************************
+     *  Build template files
+     ****************************************/
+    grunt.loadNpmTasks('grunt-bake');
+    grunt.config('bake', {
+        build: {
+            options: {},
+            files: [{
+                expand: true,
+                cwd: '<%= dirs.src.pages %>',
+                src: ['**/*.aspx'],
+                dest: '<%= dirs.dist.pages %>',
+                ext: ".aspx"
+            }]
+        }
+    });
+
+    /*****************************************
+     *  Move JS Files into place...
+     ****************************************/
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.config('copy', {
+        js: {
+            nonull: true,
+            files: [{
+                expand: true,
+                flatten: true,
+                src: ['<%= dirs.src.scripts %>**/*.js'],
+                dest: '<%= dirs.dist.scripts %>',
+                filter: 'isFile'
+            }]
+        }
+    });
+
+    /*****************************************
+     * Uglify JS
+     ****************************************/
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.config('uglify', {
+        options: {
+            mangle: false,
+            compress: {
+                drop_console: true
             },
-            dist: {
-                files: {
-                    '_dist/css/nvcg.css': '_src/StyleSheets/nvcg.scss'
-                }
-            }
+            preserveComments: false,
         },
-        bake: {
-            build: {
-                options: {
-                },
-                files: {
-                    "_dist/PageTemplates/NVCGCancerTypeHomePageTemplate.aspx":   "_src/PageTemplates/NVCGCancerTypeHomePageTemplate.aspx",
-                    "_dist/PageTemplates/NVCGHomePageTemplate.aspx":             "_src/PageTemplates/NVCGHomePageTemplate.aspx",
-                    "_dist/PageTemplates/NVCGInnerPageTemplate.aspx":             "_src/PageTemplates/NVCGInnerPageTemplate.aspx",
-                    "_dist/PageTemplates/NVCGLandingPageTemplate.aspx":           "_src/PageTemplates/NVCGLandingPageTemplate.aspx",
-                    "_dist/PageTemplates/NVCGPDQCancerInfoSummaryTemplate.aspx":  "_src/PageTemplates/NVCGPDQCancerInfoSummaryTemplate.aspx",
-                    "_dist/PageTemplates/NVCGTopicPageTemplate.aspx":             "_src/PageTemplates/NVCGTopicPageTemplate.aspx"
-                }
-            }
-        },
-        watch: {
+        js: {
+            files: [{
+                expand: true,
+                flatten: true,
+                dest: '<%= dirs.dist.scripts %>/min',
+                src: ['<%= dirs.src.scripts %>**/*.js']
+            }]
+        }
+    });
+
+    /*****************************************
+     *  Watch
+     ****************************************/
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.config('watch', {
             options: {
                 //liveReload: true
             },
             bake: {
-                files: ['_src/PageTemplates/*.aspx', '_src/PageTemplates/Includes/*.inc'],
+                files: ['<%= dirs.src.pages %>*.aspx', '<%= dirs.src.pages %>Includes/*.inc'],
                 tasks: ['bake:build']
             },
             css: {
-                files: '_src/StyleSheets/**/*.scss',
+                files: '<%= dirs.src.styles %>**/*.scss',
                 tasks: ['sass']
             }
-        },
     });
-
-    // Load Plugins
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-bake');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Tasks
     // We should ALWAYS define the 'default' task
-    grunt.registerTask('default', ['sass', 'bake:build', 'watch']);
+    grunt.registerTask('default', ['svn_fetch', 'sass', 'copy', 'bake:build', 'watch']);
+    grunt.registerTask('build', ['svn_fetch', 'sass', 'copy', 'uglify', 'bake:build']);
     // watch should run last so that it's not running over things twice
 };
+
