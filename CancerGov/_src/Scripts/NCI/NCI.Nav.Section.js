8911f7d12831c03591c2df389d2004f175e78e50
calamiasj <webcommauto@users.noreply.github.com>
2015-03-20 19:56:20 +0000

    OCEPROJECT-2205 Section nav buttons

--- CancerGov/_src/Scripts/NCI/NCI.Nav.Section.js
+++ CancerGov/_src/Scripts/NCI/NCI.Nav.Section.js
@@ -1,3 +1,18 @@
 NCI.Nav.Section = {
+sel: "#nvcgSlSectionNav .has-children > div",
+$section: $(),
+
+init: function() {
+    var _s = NCI.Nav.Section,
+        toggle = NCI.Buttons.toggle;
+
+    _s.$section = $(_s.sel);
+
+    // add +/- buttons to section nav
+    toggle.createFor(_s.$section);
+
+    _s.$section.find(".current-page > div > "+toggle.sel+", .contains-current > div > "+toggle.sel)
+        .attr("aria-expanded","true").children('span').text(toggle._innerText[toggle.lang]['true']);
+},
 
 };
