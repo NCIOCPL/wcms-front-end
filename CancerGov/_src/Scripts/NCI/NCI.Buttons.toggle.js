c38ae7544686cae1a88b7e72d39d48172e647f95
burelbachwg <webcommauto@users.noreply.github.com>
2015-03-21 21:03:33 +0000

    [OCEPROJECT-2205] Updated section navigation functionality.

--- CancerGov/_src/Scripts/NCI/NCI.Buttons.toggle.js
+++ CancerGov/_src/Scripts/NCI/NCI.Buttons.toggle.js
@@ -1,88 +1,133 @@
 NCI.Buttons.toggle = {
+	html: '<button aria-expanded="false" class="toggle" type="button"><span class="hidden">Open child elements</span></button>',
+	sel: ".toggle",
+	lang: $('html').attr('lang') || 'en', // set the language
+	_innerText: {
+		en: {'true': 'Collapse', 'false': 'Expand'},
+		es: {'true': 'Reducir', 'false': 'Ampliar'}
+	},
 
-html: '<button aria-expanded="false" class="toggle" type="button"><span class="hidden">Open child elements</span></button>',
-sel: ".toggle",
-lang: $('html').attr('lang') || 'en', // set the language
-_innerText: {
-    en: {'true': 'Collapse', 'false': 'Expand'},
-    es: {'true': 'Reducir', 'false': 'Ampliar'}
-},
+	createFor: function($target) {
+		var t = NCI.Buttons.toggle;
 
-createFor: function($target) {
-    var t = NCI.Buttons.toggle;
+		// create the button itself using jQuery
+		var $btn = $('<button>').addClass('toggle')
+			.attr({
+				'aria-expanded': 'false',
+				'type': 'button'
+			}).append(
+				$('<span>').addClass('hidden')
+					.text(t._innerText[t.lang]['false'])
+			);
 
-    // create the button itself using jQuery
-    var $btn = $('<button>').addClass('toggle')
-        .attr({
-            'aria-expanded': 'false',
-            'type': 'button'
-        }).append(
-            $('<span>').addClass('hidden')
-                .text(t._innerText[t.lang]['false'])
-        );
-    // bind the click event handler
-    $btn.on('click', t.click);
+		$btn.appendTo($target);
+		return $target.children($btn);
+	},
 
-    $btn.appendTo($target);
-},
+	clickMega: function(e) {
+		var t = NCI.Buttons.toggle;
+		e.stopPropagation();
 
-click: function(e) {
-    var t = NCI.Buttons.toggle;
-    e.stopPropagation();
+		var yes = 'true', // init true / false values
+			no = 'false',
+			sel = t.sel,
+			aria = "aria-expanded", // the aria term we're after
+			expanded = "["+aria+"='"+yes+"']", // selector for expanded items
+			collapsed = "["+aria+"='"+no+"']", // selector for collapsed items
+			$this = $(this),
+			li = $this.closest(".has-children"), // parent LI of the clicked button
+			ul = li.children("ul"), // UL menu item we are hiding / showing
+			lvl = 0;
 
-    var yes = 'true', // init true / false values
-        no = 'false',
-        sel = t.sel,
-        aria = "aria-expanded", // the aria term we're after
-        expanded = "["+aria+"='"+yes+"']", // selector for expanded items
-        collapsed = "["+aria+"='"+no+"']", // selector for collapsed items
-        $this = $(this),
-        li = $this.closest(".has-children"), // parent LI of the clicked button
-        ul = li.children("ul"), // UL menu item we are hiding / showing
-        lvl = 0;
+		if (li.hasClass("lvl-1") || li.hasClass("level-1")) { lvl = 2; }
+		if (li.hasClass("lvl-2") || li.hasClass("level-2")) { lvl = 3; }
 
-    if (li.hasClass("lvl-1") || li.hasClass("level-1")) { lvl = 2; }
-    if (li.hasClass("lvl-2") || li.hasClass("level-2")) { lvl = 3; }
+		switch($this.attr(aria)) {
+			case yes: // CLOSING
+				$this
+					.attr(aria,no).children('span').text(t._innerText[t.lang][no]).parent()
+					.find(expanded)
+						.attr(aria,no).children('span').text(t._innerText[t.lang][no]);
+					ul.slideUp("slow", Function.prototype); // Function.prototype allows us to supply a function param without creating a new one.
+				break;
+			case no: // EXPANDING
+				// collapse all the expanded siblings
+				var siblings = li.siblings(".has-children");
+				var sib_titles = siblings.children(".nav-item-title");
+				var sib_btns = sib_titles.children("button[aria-expanded='true']");
+				var sib_uls = siblings.children("ul");
 
-    switch($this.attr(aria)) {
-        case yes: // CLOSING
-            $this
-                .attr(aria,no).children('span').text(t._innerText[t.lang][yes]).parent()
-                .find(expanded)
-                    .attr(aria,no).children('span').text(t._innerText[t.lang][yes]);
-                ul.slideUp("slow", Function.prototype); // Function.prototype allows us to supply a function param without creating a new one.
-            break;
-        case no: // EXPANDING
-            // collapse all the expanded siblings
-            var siblings = li.siblings(".has-children");
-            var sib_titles = siblings.children(".nav-item-title");
-            var sib_btns = sib_titles.children("button[aria-expanded='true']");
-            var sib_spans = sib_btns.children("span");
-            var sib_uls = siblings.children("ul");
+				sib_btns.attr(aria, no).children('span').text(t._innerText[t.lang][no]);
+				sib_uls.slideUp("slow");
 
-            sib_btns.attr(aria, no);
-            sib_spans.text(t._innerText[t.lang][no]);
-            sib_uls.slideUp("slow");
+				/* li.siblings(".has-children").children(".nav-item-title").children("button[aria-expanded='true']")
+					.attr(aria, no).children('span').text(t._innerText[t.lang][no])
+					.closest(".has-children").children("ul").slideToggle("slow");
+				*/
 
-            /* li.siblings(".has-children").children(".nav-item-title").children("button[aria-expanded='true']")
-                .attr(aria, no).children('span').text(t._innerText[t.lang][no])
-                .closest(".has-children").children("ul").slideToggle("slow"); 
-            */
-            
-            // expand current pages and contains current
-            var curr_li = $(".contains-current, .current-page");
-            var curr_nit = curr_li.children(".nav-item-title");
-            var curr_btn = curr_nit.children("[" + aria + "='" + no + "']");
-            curr_btn.attr(aria, yes).children('span').text(t._innerText[t.lang][yes]);
-            curr_li.children("ul").slideDown("slow");
+				// expand current pages and contains current
+				var curr_li = $(".contains-current, .current-page");
+				var curr_nit = curr_li.children(".nav-item-title");
+				var curr_btn = curr_nit.children("[" + aria + "='" + no + "']");
+				curr_btn.attr(aria, yes).children('span').text(t._innerText[t.lang][yes]);
+				curr_li.children("ul").slideDown("slow");
 
-            // expand the one we clicked
-            $this.attr(aria, yes).children('span').text(t._innerText[t.lang][yes]);
-            // the various level <li>s themselves are hidden with CSS... 
-            li.find(".lvl-"+lvl+", .level-"+lvl).show();
-            ul.slideDown("slow");
-            break;
-    }
+				// expand the one we clicked
+				$this.attr(aria, yes).children('span').text(t._innerText[t.lang][yes]);
+				// the various level <li>s themselves are hidden with CSS...
+				li.find(".lvl-"+lvl+", .level-"+lvl).show();
+				ul.slideDown("slow");
+				break;
+		}
+	},
 
-}
+	clickSection: function(e) {
+		e.stopPropagation();
+		var t = NCI.Buttons.toggle;
+		var windowWidth = window.innerWidth || $(window).width();
+
+		var yes = 'true', // init true / false values
+			no = 'false',
+			sel = t.sel,
+			aria = "aria-expanded", // the aria term we're after
+			expanded = "["+aria+"='"+yes+"']", // selector for expanded items
+			collapsed = "["+aria+"='"+no+"']", // selector for collapsed items
+			$this = $(this),
+			li = $this.closest(".has-children"), // parent LI of the clicked button
+			ul = li.children("ul"), // UL menu item we are hiding / showing
+			lvl = 0;
+
+		if (li.hasClass("lvl-1") || li.hasClass("level-1")) { lvl = 2; }
+		if (li.hasClass("lvl-2") || li.hasClass("level-2")) { lvl = 3; }
+
+		switch($this.attr(aria)) {
+			case yes: // CLOSING
+				$this
+					.attr(aria,no).children('span').text(t._innerText[t.lang][no]).parent()
+					.find(expanded)
+						.attr(aria,no).children('span').text(t._innerText[t.lang][no]);
+					ul.slideUp('slow', Function.prototype); // Function.prototype allows us to supply a function param without creating a new one.
+				break;
+			case no: // EXPANDING
+				// collapse all the expanded siblings
+				var siblings = li.siblings(".has-children");
+				var sib_titles = siblings.children("div");
+				var sib_btns = sib_titles.children("button[aria-expanded='true']");
+				var sib_uls = siblings.children("ul");
+
+				// close any open siblings and their children
+				sib_btns.attr(aria, no).children('span').text(t._innerText[t.lang][no]);
+				if(windowWidth <= 1024) {
+					sib_uls.slideUp('slow', Function.prototype);
+				} else {
+					sib_urls.not('.section-nav .contains-current > ul').slideUp('slow', Function.prototype);
+					sib_urls.filter('.section-nav .contains-current > ul').css('display', 'none');
+				}
+
+				// expand the one we clicked
+				$this.attr(aria, yes).children('span').text(t._innerText[t.lang][yes]);
+				ul.slideDown('slow', Function.prototype);
+				break;
+		}
+	}
 };
--- CancerGov/_src/Scripts/NCI/NCI.Nav.Section.js
+++ CancerGov/_src/Scripts/NCI/NCI.Nav.Section.js
@@ -1,18 +1,85 @@
 NCI.Nav.Section = {
-sel: "#nvcgSlSectionNav .has-children > div",
-$section: $(),
+	sel: '.section-nav',
+	selWithChildren: '.section-nav .has-children',
+	idOpenerButton: 'section-menu-button',
 
-init: function() {
-    var _s = NCI.Nav.Section,
-        toggle = NCI.Buttons.toggle;
+	$section: $(),
+	$withChildren: $(),
+	$openerButton: $(),
 
-    _s.$section = $(_s.sel);
+	init: function() {
+		var _s = NCI.Nav.Section,
+			toggle = NCI.Buttons.toggle;
 
-    // add +/- buttons to section nav
-    toggle.createFor(_s.$section);
+		_s.$section = $(_s.sel);
 
-    _s.$section.find(".current-page > div > "+toggle.sel+", .contains-current > div > "+toggle.sel)
-        .attr("aria-expanded","true").children('span').text(toggle._innerText[toggle.lang]['true']);
-},
+		if(_s.$section) {
+			// make the button to open the section nav
+			_s.$openerButton = $('<a>')
+				.attr('id', _s.idOpenerButton)
+				.attr('href', '#')
+				.text('Section Menu')
+				.on('click', _s.onOpenerClick)
+				.insertAfter('.fixedtotop');
+			// add margin to prevent the button from overlaying the page header
+			$('.contentzone').addClass('has-section-nav');
 
+			_s.$withChildren = $(_s.selWithChildren + '> div');
+
+			// add +/- buttons to section nav
+			toggle.createFor(_s.$withChildren.not('.level-0 > div')).on('click', toggle.clickSection);
+
+			_s.$withChildren.parent('li').find("div.current-page > " + toggle.sel + ", .contains-current > div > " + toggle.sel)
+				.attr("aria-expanded", "true").children('span').text(toggle._innerText[toggle.lang]['true']);
+
+
+			/* showing/hiding the section navigation when switching between desktop and mobile */
+			var curWidth = window.innerWidth || $(window).width(),
+			oldWidth = curWidth;
+
+			$(window).resize(function() {
+				curWidth = window.innerWidth || $(window).width();
+				if(oldWidth > 1024 && curWidth <= 1024) {
+					/* if we've gone from desktop to mobile */
+					// hide the setion navigation
+					_s.$section.hide();
+				} else if (oldWidth <= 1024 && curWidth > 1024) {
+					/* if we're at a desktop resolution */
+					// remove the overlay
+					$('#overlay').remove();
+					// show the section navigation
+					_s.$section.show();
+					// remove some mobile-only classes
+					_s.$section.removeClass('open');
+					_s.$openerButton.removeClass('open');
+				}
+				oldWidth = curWidth;
+			}).trigger('resize');
+		}
+	},
+
+	onOpenerClick: function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+
+		var _s = NCI.Nav.Section;
+
+		// slide up section nav
+		_s.$section.slideToggle(200, function() {
+			// remove open class from button
+			_s.$openerButton.toggleClass('open', $(this).is(':visible'));
+			_s.$section.toggleClass('open', $(this).is(':visible'));
+			if(_s.$section.is(':visible')) {
+				/* section nav is OPEN */
+				// append overlay div to content area for grey page overlay
+				$('#content').append('<div id="overlay"></div>');
+				// enable clicking outside the section-menu to close
+				$('#overlay').click(_s.onOpenerClick);
+			} else {
+				/* section nav is CLOSED */
+				// remove grey overlay div
+				$('#overlay').remove();
+			}
+		});
+	}
 };
