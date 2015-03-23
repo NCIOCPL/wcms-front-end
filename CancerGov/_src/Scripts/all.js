2cf01b63ae0d8225c4829d295186a09da998faec
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-03-23 18:44:25 +0000

    Updating BuildOTP(): https://tracker.nci.nih.gov/browse/OCEPROJECT-2670

--- CancerGov/_src/Scripts/all.js
+++ CancerGov/_src/Scripts/all.js
@@ -408,5 +408,8 @@ jQuery(document).ready(function(jQuery) {
 		accordionize();
 	})(jQuery);
 	/*** END accordionizer ***/
-	
+
+	// Run this script to dynamically generate an "On This Page" block at the top of the page if it's specified the HTML
+	// TODO: move this to a different location when cleaning up the JavaScript for a future release.
+	NCI.buildOTP();
 });
--- CancerGov/_src/Scripts/insidepages-sharecomponent.js
+++ CancerGov/_src/Scripts/insidepages-sharecomponent.js
@@ -2,7 +2,3 @@ $(".shareComponent").click(function () {
 	$(this).find(".shareWindow").toggleClass("hide");
 	$(this).find(".shareBtn").toggleClass("shareBtnOpen");
 });
-
-// Run the script to dynamically generate an "On This Page" block at the top of the page if it's specified the HTML
-// TODO: move this to a different location when cleaning up the JavaScript for a future release.
-NCI.buildOTP();
