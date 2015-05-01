11e4f294b05d98eac1fac29087876da2a8393dec
Blair Learn <blairlearn@users.noreply.github.com>
2015-05-01 20:53:51 +0000

    Merge Devon Rex changes for CancerGov.

---
+++ CancerGov/_src/Scripts/all-mobilemenu.js
@@ -0,0 +1,67 @@
+// write the mobile menu
+$(document).ready(function() {
+    var $mobileNav = $('#mega-nav > .nav-menu');
+
+    // Global mobile nav functionality
+    var styleMobileNav = function() {
+        var active = $(".active").parents(".nav-item");
+        var toggles = $('#mega-nav .toggle');
+        toggles.filter('[aria-expanded="false"]')
+            .parent('div').parent('li').children('ul').hide()
+            .parent(".nav-item").find("[aria-expanded='true']").attr("aria-expanded","false");
+        // highlight active items
+        $("#mega-nav li li > div > .toggle[aria-expanded='true']").closest("li").addClass("highlight");
+        $("#mega-nav .highlight").not(active).removeClass("highlight").children("ul").hide();
+        $('#mega-nav .nav-menu > li:first-child').addClass("highlight");
+
+        $('#mega-nav div > a').on('mousedown mouseup mouseleave touchstart touchend touchcancel', function(e) {
+            $(this).toggleClass('active', e.type === 'mousedown' || e.type === 'touchstart');
+            $(this).parent('div').toggleClass('active', e.type === 'mousedown' || e.type === 'touchstart');
+        });
+    };
+
+    var functionMobileNav = function() {
+        // Add toggle functionality
+        $('#mega-nav .toggle').click(function(event) {
+            event.stopPropagation();
+            var t = $(this);
+            var closest = t.closest("li");
+            var aria_expanded = t.attr('aria-expanded');
+            // If the toggle is open, do this
+            if (aria_expanded == 'true' ) {
+                closest.find("button[aria-expanded='true']").closest("li").children("ul").slideToggle("slow", function() {
+                    //Animation complete
+                });
+                closest.find(".toggle").attr('aria-expanded','false');
+                // remove highlight class
+                closest.removeClass("highlight");
+                // add highlight back to parent
+                closest.parent('ul').parent('li').addClass('highlight');
+                // Stop processing
+                return;
+            }
+            else if (aria_expanded == 'false' ) { // If the toggle is closed, do this
+                // close any open siblings and their children...
+                closest.siblings().children("div").children("button[aria-expanded='true']").closest("li").children("ul").slideToggle( "slow", function() {
+                    //Animation complete
+                });
+                // ...and add proper ARIA to indicate those siblings and children are closed
+                closest.siblings().children("div").children("button").attr('aria-expanded','false');
+                // slide open list of nav elements for selected button
+                closest.children("ul").slideToggle( "slow", function() {
+                    // Animation complete.
+                });
+                // add ARIA to indicate this section has been opened
+                t.attr('aria-expanded','true');
+                // remove highlight class from any other items
+                $("#mega-nav li").removeClass("highlight");
+                // add highlight class to this item
+                closest.addClass("highlight");
+                return;
+            }
+        });
+    };
+
+    styleMobileNav();
+    functionMobileNav();
+});
---
+++ CancerGov/_src/Scripts/cthp-cards.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function(jQuery) {
+	/*** BEGIN CTHP Page Options
+	 * Place mobile page options below the h1.
+	 ***/
+	(function($) {
+		if($('h1').length > 0) {
+			// if there is an h1, we are not on the home page
+			$('h1').after(
+				$(document.createElement('div'))
+				.addClass('cthp-mobile')
+				.addClass('page-options')
+				.prepend(
+					$('.page-options > ul').clone()
+				)
+			);
+		}
+	})(jQuery);
+	/*** END CTHP Page Options ***/
+	
+	/*** BEGIN CTHP Cards
+	 * Wrap pairs of cards in a row.
+	 ***/
+	(function($) {
+		var lis = $(".cthpCard");
+		for(var i = 0; i < lis.length; i+=2) {
+			lis.slice(i, i+2).wrapAll("<div class='row' data-match-height></div>");
+		}
+	})(jQuery);
+	/*** END CTHP Cards ***/
+});
