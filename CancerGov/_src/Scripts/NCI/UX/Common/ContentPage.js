6f29e142a4c0c1ad9d1a19022e8b2549de76fd42
Mark Cramer <markecramer@users.noreply.github.com>
2016-01-20 21:23:14 +0000

    OCEPROJECT-3109 - Added javascript to check for an empty div in the mega menu and assign any matching items with a class of .empty-mega. This is then used in the CSS to remove padding and background color of that empty div.

--- CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
@@ -76,6 +76,17 @@ define(function(require) {
 			$(window).resize(viewportHeight);
 		})(jQuery);
 		/*** END Mega Menu init ***/
+		
+		/*** BEGIN Mega Menu empty class creation 
+		*** create a class for mega menu items that have no actual content, so we can unformat them  ***/
+		jQuery(document).ready(function(jQuery) {
+			$(".sub-nav-mega").each(function(){
+				if (!$(this).text().trim().length) {     
+				$(this).addClass("empty-mega");
+				}
+			})
+		});
+		/*** END Mega Menu empty class creation 
 
 		/*** BEGIN dictionary toggle ***/
 		(function($) {
