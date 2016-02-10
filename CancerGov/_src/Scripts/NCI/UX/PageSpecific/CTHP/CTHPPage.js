b66a5afde1433fc7c5ac1458cfb9a9bd9ada8d77
Volker Englisch <venglisch@users.noreply.github.com>
2016-02-10 17:15:09 +0000

    Hack to identify and style audience for CTHP cards. OCEPROJECT-3953

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
@@ -1,17 +1,7 @@
 define(function(require) {
 	require('CTHP/Enhancements/cthp_cards');
 	require('Common/Enhancements/equal_heights');
-    var myUrl = window.location.href;
 
-    /* Need to identify HP/patient cards to style patient card differently */
-    /* Using the current page's URL since there is no other mechanism      */
-    /* available to identify the audience.                                 */
-    /* This hack should be removed when an audience meta tag has been      */
-    /* provided                                                            */
-    /* ------------------------------------------------------------------- */
-    if (myUrl.search("/types/") > 0 || myUrl.search("/tipos/") > 0) {
-       if (myUrl.search("/hp") == -1 && myUrl.search("/pro") == -1) {
-        $('div.cthp-content').addClass('cthp-patient-content');
-       }
-    }
+    /* Hack to identify CTHP audience (HP/patient) for styling purposes */
+	require('Patches/Hotfixes/OCEPROJECT-3953');
 });
