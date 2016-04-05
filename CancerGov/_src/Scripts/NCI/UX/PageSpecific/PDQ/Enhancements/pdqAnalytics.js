ad2b8775e65299793479dbfabff63cb0225a565f
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-04-05 20:53:01 +0000

    //tracker.nci.nih.gov/browse/WCMSFEQ-62 - adding Javascript for "On This Page" analytics tracking

---
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/PDQ/Enhancements/pdqAnalytics.js
@@ -0,0 +1,36 @@
+define(function(require) {
+	var $ = require('jquery');
+
+	function _initialize() {
+		// Track clicks on "On This Page" links
+		$('.on-this-page.show ul li').each(function(i, el) {
+			$(el).on('click', 'a', function(event) {
+				var $this = $(this);
+				var pageTitle = $('h1 span').first().text();
+				var linkText = $this.text();	
+				NCIAnalytics.OnThisPageClick($this, pageTitle, linkText);
+			});
+		});		
+	}
+
+	/**
+	 * Identifies if this enhancement has been initialized or not.
+	 * @type {Boolean}
+	 */
+	var initialized = false;
+
+	/**
+	 * Exposed functions available to this module.
+	 */
+	return {
+		init: function() {
+			if (initialized) {
+				return;
+			}
+
+			_initialize();
+
+			initialized = true;
+		}
+	};
+});
--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/PDQ/PDQPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/PDQ/PDQPage.js
@@ -1,3 +1,7 @@
 define(function(require) {
+	var $ = require('jquery');
 	require('PDQ/pdqcis');
+	$(document).ready(function($) {
+		require('PDQ/Enhancements/pdqAnalytics').init();
+	});
 });
