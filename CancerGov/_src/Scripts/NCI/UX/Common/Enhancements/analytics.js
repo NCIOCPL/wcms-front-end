eb1186df47b15a249930ef904e006518a65e80de
Frank Ali <alifr@users.noreply.github.com>
2016-04-14 18:10:35 +0000

    [WCMSFEQ-156][WCMSFEQ-104][WCMSFEQ-72] Adding analytics namespace to all events. Adding analytics for font resizer. Updating mega menu reveal script to prevent reporting on casual mouseovers. Adding analytics for Section Menu click.

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/analytics.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/analytics.js
@@ -23,10 +23,11 @@ define(function(require) {
 
 		$('#mega-nav a')
 			.filter(function() { return $(this).closest('.mobile-item').length === 0; })
-			.on('click', function(event) {
-				var $this = $(this);
-				var tree = [];
-				var treeParents = $this.parent('li').parents('li');
+			.on('click.analytics', function(event) {
+				var $this = $(this),
+					tree = [],
+					treeParents = $this.parent('li').parents('li')
+				;
 				tree.push($this[0]);
 				if (treeParents.children('a').length > 0) {
 					tree.push(treeParents.children('a')[0]);
@@ -41,24 +42,25 @@ define(function(require) {
 		var menuRevealed;
 		var megaNav = $("#mega-nav");
 		var mobileMenuBar = $(".mobile-menu-bar");
-		megaNav.on('mouseenter','.nav-item',function(e){
+		megaNav.on('mouseenter.analytics','.nav-item',function(e) {
 			window.clearTimeout(menuRevealed);
-			if(mobileMenuBar.is(":hidden")) {
+			if (mobileMenuBar.is(":hidden")) {
 				menuRevealed = window.setTimeout(function () {
 					var menuText = $('#mega-nav a.open').text();
 					NCIAnalytics.MegaMenuDesktopReveal(this, menuText);
 				}, 1000);
 			}
-
-		}).on('click','button.toggle',function(){
+		}).on('mouseleave.analytics','.nav-item',function(e){
+			window.clearTimeout(menuRevealed);
+		}).on('click.analytics','button.toggle',function(){
 			var $this = $(this),
 				isExpanded = $this.attr('aria-expanded')==='true',
 				tree = treeText($this.parents("li")).string,
 				linkText = $this.prev().text() //linkText no longer used now that it's being captured with the tree values
-				;
+			;
 			NCIAnalytics.MegaMenuMobileAccordionClick(this, isExpanded, tree);
 
-		}).on('click','.lvl-1 a, .mobile-item a',function(e){
+		}).on('click.analytics','.lvl-1 a, .mobile-item a',function(e){
 			if(mobileMenuBar.is(":visible")){
 				//e.preventDefault();
 				var $this = $(this),
@@ -69,14 +71,14 @@ define(function(require) {
 					heading = $.trim(root.children(":first").find('a').text()),
 					parent = $this.closest(".lvl-2"),
 					subHeading = parent[0] && parent.children(":first").find('a').get(0) !== this?$.trim(parent.children(":first").find('a').text()):heading
-					;
+				;
 
 				//console.log("url: " + url + "\nlinkText: " + linkText  + "\nlinkUrl: " + linkUrl + "\nheading: " + heading + "\nsubHeading: " + subHeading);
 				NCIAnalytics.MegaMenuMobileLinkClick(this, url, linkText, linkUrl, heading, subHeading);
 			}
 		});
 
-		mobileMenuBar.on('click','.nav-header',function(){
+		mobileMenuBar.on('click.analytics','.nav-header',function(){
 			var isVisible = false;
 			if($('#mega-nav > .nav-menu').attr('aria-hidden') === 'false'){
 				isVisible = true;
@@ -86,11 +88,8 @@ define(function(require) {
 			}
 		});
 
-
-
-
 		$('.utility a').each(function(i, el) {
-			$(el).on('click', function(event) {
+			$(el).on('click.analytics', function(event) {
 				var $this = $(this);
 				var linkText = $this.text();
 
@@ -99,12 +98,12 @@ define(function(require) {
 		});
 
 		$('.nci-logo')
-			.on('click', function(event) {
+			.on('click.analytics', function(event) {
 				NCIAnalytics.LogoClick(this)
 			});
 
 		$('.feature-primary .feature-card').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.children('h3').text();
 				var linkText = $this.children('h3').text();
@@ -116,7 +115,7 @@ define(function(require) {
 		});
 
 		$('.feature-secondary .feature-card').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.children('h3').text();
 				var linkText = $this.children('h3').text();
@@ -128,7 +127,7 @@ define(function(require) {
 		});
 
 		$('.guide-card .card').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $(el).children('h2').text();
 				var linkText = $this.text();
@@ -140,7 +139,7 @@ define(function(require) {
 		});
 
 		$('.multimedia .card').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.children('h3').text();
 				var linkText = $this.children('h3').text();
@@ -152,7 +151,7 @@ define(function(require) {
 		});
 
 		$('.card-thumbnail .card-thumbnail-image').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.closest('a').attr('data-title');
 				var linkText = 'Image';
@@ -164,7 +163,7 @@ define(function(require) {
 		});
 
 		$('.card-thumbnail .card-thumbnail-text').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.closest('h3').find('a:first').text();
 				var linkText = $this.closest('h3').find('a:first').text();
@@ -176,7 +175,7 @@ define(function(require) {
 		});
 
 		$('.cthp-card-container .cthpCard').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				var cardTitle = $this.closest('.cthpCard').find('h3:first').text();
 				var linkText = $this.text();
@@ -189,7 +188,7 @@ define(function(require) {
 
 		// Track clicks on on Topic Page Featured Card
 		$('.topic-feature .feature-card').each(function(i, el) {
-			$(el).on('click', 'a', function(event) {
+			$(el).on('click.analytics', 'a', function(event) {
 				var $this = $(this);
 				// if the card is inside the intro text or body then it is an inline card
 				var isInline = $this.parents("#cgvBody,#cgvIntroText")[0];
@@ -202,7 +201,7 @@ define(function(require) {
 			});
 		});
 
-		$("#nvcgSlSectionNav a").on("click",function(event){
+		$("#nvcgSlSectionNav a").on('click.analytics',function(event){
 			//event.preventDefault(); //uncomment when testing link clicks
 			var $this = $(this),
 				url = 'www.cancer.gov' + location.pathname.toLowerCase(),
@@ -215,9 +214,57 @@ define(function(require) {
 			//console.log("url: " + url + "\nheading: " + heading  + "\nparent: " + parent + "\nlinkText: " + linkText + "\ndepth: " + depth);
 			NCIAnalytics.SectionLinkClick(this,url,heading,linkText,depth,parent);
 		});
+
+		$('.add_this_btn').each(function() {
+			var $this = $(this);
+			$this.parent().on('click.analytics', $this, function(e) {
+				NCIAnalytics.BookmarkShareClick(this);
+			});
+		});
+
+		$('.po-font-resize a').on('click.analytics',function(e){
+			console.log("clicked!");
+			var $this = $(this);
+			//reset the mouseleave event on each click so it only reports once
+			$this.off('mouseleave.analytics');
+
+			//report the final font size on mouse leave of the icon
+			$this.on('mouseleave.analytics',function(){
+				//report font size after clicking is completed
+				var target = $(".resize-content:first"),
+					fontStyle = target.css("font-size"),
+					fontSize
+					;
+				console.log(fontStyle);
+
+				switch (fontStyle) {
+					case '19.2px':
+						fontSize = 'Medium';
+						break;
+					case '23.04px':
+						fontSize = 'Large';
+						break;
+					case '27.648px':
+						fontSize = 'Extra Large';
+						break;
+					default:
+						fontSize = 'Normal';
+				}
+				NCIAnalytics.fontResizer(this,fontSize);
+
+				//unbind the mouseleave event to prevent reporting on casual mouseovers
+				$this.off('mouseleave.analytics');
+			});
+
+		});
+
+
 		//analytics for components generated by JavaScript
 		$(window).load(function(){
-			$("#nvcgSlSectionNav button.toggle").on("click",function(event){
+
+			NCIAnalytics.fontResizer(this,'Normal',true);
+
+			$("#nvcgSlSectionNav button.toggle").on('click.analytics',function(event){
 
 				var $this = $(this),
 					url = 'www.cancer.gov' + location.pathname.toLowerCase(),
@@ -225,7 +272,7 @@ define(function(require) {
 					heading = $.trim(root.children(":first").text()),
 					tree = treeText($this.parents("li:not(.level-0)")).string,
 					isExpanded = $this.attr("aria-expanded") == "true"
-					;
+				;
 
 				//console.log("url: " + url + "\nisExpanded: " + isExpanded  + "\nheading: " + heading  + "\nparent: " + parent + "\nevent: " + event.type);
 				NCIAnalytics.SectionAccordionClick(this,url,isExpanded,heading,tree);
@@ -233,22 +280,22 @@ define(function(require) {
 			$(".back-to-top").one("reveal",function(e){
 				NCIAnalytics.BackToTopReveal(this,true);
 
-			}).on("click",function(e){
+			}).on('click.analytics',function(e){
 				var UtilityBar = $("#nvcgSlUtilityBar").is(":visible");
 				NCIAnalytics.BackToTopClick(this,UtilityBar);
 			});
-		});
-	})();
 
 
-	// AddThis overrides the 'onclick' event handlers, so re-bind analytics after AddThis loads.
-	(function() {
-		$('.add_this_btn').each(function() {
-			var thisBtn = this;
-			var $this = $(this);
-			$this.parent().on('click', $this, function(e) {
-				NCIAnalytics.BookmarkShareClick(thisBtn);
+			$('#section-menu-button').on('click.analytics',function(e){
+				var sectionNav = $(".section-nav"),
+					sectionTitle = $.trim(sectionNav.find(".current-page").text())
+				;
+				if(!sectionNav.is(".open")){
+					NCIAnalytics.SectionMenuButtonClick(this,sectionTitle);
+				}
 			});
+
 		});
+
 	})();
 });
