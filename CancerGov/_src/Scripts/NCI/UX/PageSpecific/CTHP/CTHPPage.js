dc24f836846303090a5088e6c97c7b1bcc788747
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-04-21 21:12:48 +0000

    //tracker.nci.nih.gov/browse/WCMSPERC-170 - Changed Overview seection width to 100% - section will now span 2 cards wide, not 2 cards high - Turned off doublehigh-resize JS function

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
@@ -1,7 +1,4 @@
 define(function(require) {
 	require('CTHP/Enhancements/cthp_cards');
-	$(function() {
-		require('CTHP/Enhancements/doublehigh-expander').init();
-	});
 	require('Common/Enhancements/equal_heights');
 });
