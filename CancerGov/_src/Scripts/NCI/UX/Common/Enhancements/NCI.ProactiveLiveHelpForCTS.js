9d7c2222d60222e6140e992f12dc047a7c311681
Blair Learn <blairlearn@users.noreply.github.com>
2016-06-06 21:19:04 +0000

    Change proactive chat window to be opened directly instead of using live help code.

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.ProactiveLiveHelpForCTS.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.ProactiveLiveHelpForCTS.js
@@ -1,26 +1,21 @@
 define(function(require){
 	
 	var $ = require('jquery');
-
-	// Load all the scripts for the actual chat mechanism.  The 'chat-main' alias
-	// and enforcement of load-order is done through a shim-config (with paths)
-	// in the main config.js.
-	require('chat-main');
 	
 	
 	var CTS_URLS = [
-		"/grants-training/grants-process/mechanisms/grants",
-		"/about-cancer/treatment/clinical-trials/basic"
+		"/about-cancer/treatment/clinical-trials/basic",
+		"/about-cancer/treatment/clinical-trials/search"
 	];
 
 	// Which chat server should be used? Test or production.
 	var HOST_SERVER="nci--tst.custhelp.com";
 
 	var POPUP_DELAY_SECONDS = 5;	// Number of seconds to delay before displaying the popup..
-	var POPUP_TITLE	= "Chat Online";
-	var POPUP_MESSAGE = "\u003Cp\u003ENeed help? Would you like to speak to an NCI Information Specialist about finding a clinical trial?\u003C\/p\u003E";
-	var POPUP_WIDTH = 300;
-	var POPUP_HEIGHT = 300;
+	var POPUP_TITLE	= "Need Help?";
+	var POPUP_MESSAGE = "\u003Cp\u003EWould you like to speak to an NCI Information Specialist about finding a clinical trial?\u003C\/p\u003E";
+	var PROMPT_WIDTH = 400;
+	var PROMPT_HEIGHT = 200;
 
 	var POPUP_WINDOW_ID = "ProactiveLiveHelpForCTSPrompt";	// Pop up window element's ID.
 
@@ -36,39 +31,34 @@ define(function(require){
 	// would like to do a chat with a Live Help Specialist.
 	function _setupPrompt() {
 		// Scroll to top.
-		$("html, body").animate({scrollTop: 0}, "slow");
+		//$("html, body").animate({scrollTop: 0}, "slow");
 
-					 // <form class=''><input id='rn_History_Field' type='hidden'/></form>
-		var popupBody = "\u003Cform class=''\u003E\u003Cinput id='rn_History_Field' type='hidden'/\u003E\u003C/form\u003E"
-			+ POPUP_MESSAGE
+		var popupBody = POPUP_MESSAGE
 			// <form id="rn_ChatLaunchFormOpen_2_Form" onsubmit="return false;">
 			+ "\u003Cform id=\u0022rn_ChatLaunchFormOpen_2_Form\u0022 onsubmit=\u0022return false;\u0022\u003E"
-			//<div id="rn_nciChatLaunchButton_4">
-			+ "\u003Cdiv id=\u0022rn_nciChatLaunchButton_4\u0022\u003E"
-			//<input id="rn_nciChatLaunchButton_4_Button" type="image"  class="chat-buttons" name="cancer-info" src="/euf/assets/themes/nci/nci-img/quit-smoking-button.gif" title="Smoking Cessation Assistance" alt="Smoking Cessation Assistance"></input>
-			+ "\u003Cp\u003E\u0026nbsp;\u003C\/p\u003E\n\u003Cp id=\u0022rn_nciChatLaunchButton_4_Button\u0022\u003E\n\t\u003Cinput type=\u0022button\u0022 name=\u0022rn_nciChatLaunchButton_4_Button\u0022 class=\u0022chat-buttons\u0022 value=\u0022Chat Now\u0022 \u003E\n\u003C\/p\u003E"
+			//<div>
+			+ "\u003Cdiv\u003E"
+			//<input id="chat-button" type="image"  class="chat-buttons" name="cancer-info" src="/euf/assets/themes/nci/nci-img/quit-smoking-button.gif" title="Smoking Cessation Assistance" alt="Smoking Cessation Assistance"></input>
+			+ "\u003Cp id=\u0022rn_nciChatLaunchButton_4_Button\u0022\u003E\n\t\u003Cinput id=\u0022chat-button\u0022 type=\u0022button\u0022 name=\u0022rn_nciChatLaunchButton_4_Button\u0022 class=\u0022chat-buttons\u0022 value=\u0022Chat Now\u0022 \u003E\n\u003C\/p\u003E"
 			// </div></form>
 			+ "\u003C/div\u003E\u003C/form\u003E";
 		
 		// Create the pop up.
-		$('body').append("<div id='" + POPUP_WINDOW_ID + "'><a id='popup-message-close'>X</a><br /><h1 class='popup-message-title'>" + POPUP_TITLE + "</h1><div id='popup-message-content'>" + popupBody + "</div></div><div id='popup-message-background'></div>");
+		$('body').append("<div id='" + POPUP_WINDOW_ID + "'><a id='popup-message-close'>X</a><br /><h1 class='popup-message-title'>" + POPUP_TITLE + "</h1><div id='popup-message-content'>" + popupBody + "</div></div>");
 		
-		_connectChatToButton();
-		
-		// Attach click handler here.
-		// In the for-real version, we'll probably rename "chat_button" to rn_nciChatLaunchButton_4_Button and
-		// let the chat frameowrk attach to that as the trigger button.
-		//jQuery("#cheat_button").click(function() {jQuery("#rn_nciChatLaunchButton_4_Button").click(); jQuery(this).unbind('click')});
-		//jQuery("#rn_nciChatLaunchButton_4_Button").click(function() {alert("This is where the chat dialog would appear."); jQuery(this).unbind('click')});
+		//_connectChatToButton();
+		$("#chat-button").click(function(){
+			window.open("https://nci--tst.custhelp.com/app/chat/chat_landing?_icf_22=92", "ProactiveLiveHelpForCTS", "height=600,width=633");
+			_dismissPrompt();
+		});
 		
 		// Center and display the pop up.
-		_centerPrompt(POPUP_WIDTH, POPUP_HEIGHT);
+		_centerPrompt(PROMPT_WIDTH, PROMPT_HEIGHT);
 		_displayPrompt();
 		
 		// Set up event handlers for the various ways to close the pop up
 		$("#popup-message-close").click(function() { _dismissPrompt(); });
-		$("#popup-message-background").click(function() { _dismissPrompt(); });
-		$(document).keypress(function(e) { if( e.keyCode == 27 && popupStatus == true) _dismissPrompt(); });
+		$(document).keypress(function(e) {if( e.keyCode == 27 && popupStatus == true) _dismissPrompt();});
 	}
 	
 	function _centerPrompt(width, height) {
@@ -120,6 +110,7 @@ define(function(require){
 		}
 	}
 
+	// Allows the "Right Now" chat functionality to connect to the chat button.
 	function _connectChatToButton() {
 		RightNow.Url.setParameterSegment(5);
 		RightNow.Interface.Constants =
@@ -127,12 +118,18 @@ define(function(require){
 		YAHOO.util.Event.throwErrors = true;
 		RightNow.Interface.setMessagebase(function(){return {"WARNING_LBL":"Warning","INFORMATION_LBL":"Information","HELP_LBL":"Help","ERROR_REQUEST_ACTION_COMPLETED_MSG":"There was an error with the request and the action could not be completed.","OK_LBL":"OK","DIALOG_LBL":"dialog","DIALOG_PLEASE_READ_TEXT_DIALOG_MSG_MSG":"dialog, please read above text for dialog message","BEG_DIALOG_PLS_DISMISS_DIALOG_BEF_MSG":"Beginning of dialog, please dismiss dialog before continuing","END_DIALOG_PLS_DISMISS_DIALOG_BEF_MSG":"End of dialog, please dismiss dialog before continuing","ERR_SUBMITTING_FORM_DUE_INV_INPUT_LBL":"There was an error submitting the form due to invalid input","ERR_SUBMITTING_SEARCH_MSG":"There was a problem with your request.  Please change your search terms and try again.","CLOSE_CMD":"Close","BACK_LBL":"Back","FOLLOWING_WIDGET_JAVASCRIPT_SYNTAX_MSG":"The following widget JavaScript either has a syntax error or its class name is not correct: ","YOU_HAVE_PCT_D_WARNINGS_PAGE_LBL":"You have %d warnings on this page","YOU_HAVE_ONE_WARNING_PAGE_LBL":"You have 1 warning on this page.","YOU_HAVE_PCT_D_ERRORS_PAGE_LBL":"You have %d errors on this page.","YOU_HAVE_ONE_ERROR_PAGE_LBL":"You have 1 error on this page.","CHAT_IS_NOT_AVAILABLE_MSG":"Chat is not available."};});
 		RightNow.Interface.setConfigbase(function(){return {"DE_VALID_EMAIL_PATTERN":"^(([-!#$%&'*+\/=?^~`{|}\\w]+(\\.[-!#$%&'*+\/=?^~`{|}\\w]+)*)|(\"[^\"]+\"))@[0-9A-Za-z]+(-[0-9A-Za-z]+)*(\\.[0-9A-Za-z]+(-[0-9A-Za-z]+)*)+$","CP_HOME_URL":"home","CP_FILE_UPLOAD_MAX_TIME":300,"OE_WEB_SERVER":HOST_SERVER};});
-		YAHOO.util.Event.onDOMReady(function(){this.Dialog.setRenderDiv(); this.prepareVirtualBufferUpdateTrigger(); this.ariaHideFrames();}, null, RightNow.UI);
-		YAHOO.util.Event.onDOMReady(function(){
+		//YAHOO.util.Event.onDOMReady(function(){this.Dialog.setRenderDiv(); this.prepareVirtualBufferUpdateTrigger(); this.ariaHideFrames();}, null, RightNow.UI);
+		//YAHOO.util.Event.onDOMReady(function(){
+		//YAHOO.util.Event.onDOMReady(function(){
+			RightNow.UI.Dialog.setRenderDiv();
+			RightNow.UI.prepareVirtualBufferUpdateTrigger();
+			RightNow.UI.ariaHideFrames();//}, null, RightNow.UI);
+		//YAHOO.util.Event.onDOMReady(function(){
 		var W = RightNow.Widget,
 		c = 'createWidgetInstance';
+		
 		W[c]({"i":{"c":"nciChatLaunchButton","n":"nciChatLaunchButton_4","w":4},"a":{"label_button":"Smoking Cessation Assistance","loading_icon_path":"images\/indicator.gif","chat_landing_page":"\/app\/chat\/chat_landing","launch_width":380,"launch_height":560,"error_location":"rn_ErrorLocation","open_in_new_window":true,"protocol_selection":"auto","post_contact_fields":false,"button_type":"quitting_smoking"},"j":{"chatWindowName":"nci__tst1","baseUrl":"https:\/\/" + HOST_SERVER,"isLoggedIn":false,"buttonType":"quitting_smoking","disabled_img":"\/euf\/assets\/themes\/nci\/nci-img\/cis-livehelp-quitsmoking-onlick-button.gif","img":"\/euf\/assets\/themes\/nci\/nci-img\/quit-smoking-button.gif"}},'nciChatLaunchButton_4','custom/chat/nciChatLaunchButton','nciChatLaunchButton','4',true);
-		RightNow.Url.transformLinks();});
+		RightNow.Url.transformLinks();//});
 
 		var _rnq=_rnq||[];_rnq.push({"debug":true,"s":"VYzjzadm","uh":"af25197a","uc":HOST_SERVER + "\/app\/chat\/chat_launch","b":"ca99729","i":"nci__tst:nci","f":"rnw","p":"Customer Portal","v":"14.2.0.1-b131-sp5","th":"www.rnengage.com"});
 		(function(f){var b,d,a=document.createElement("iframe"),c=document.getElementsByTagName("script");a.src="javascript:false";a.title="";a.role="presentation";(a.frameElement||a).style.cssText="width:0;height:0;border:0";c=c[c.length-1];c.parentNode.insertBefore(a,c);try{b=a.contentWindow.document}catch(g){d=document.domain,a.src="javascript:var d=document.open();d.domain='"+d+"';void(0);",b=a.contentWindow.document}b.open()._l=function(){for(var a;f.length;)a=this.createElement("script"),d&&(this.domain=
