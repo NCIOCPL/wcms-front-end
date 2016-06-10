3f544dd42ec84d74c33123f734d88e7e51fb8f95
Mark Cramer <markecramer@users.noreply.github.com>
2016-06-10 15:06:36 +0000

    [OCEPROJECT-4082] [OCEPROJECT-4087] Reversing changes made in Revision 15981 due to form submission errors.

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Search/Enhancements/BasicCTSSearchFormSetup.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Search/Enhancements/BasicCTSSearchFormSetup.js
@@ -1,67 +1,23 @@
 define(function(require) {
 	var $ = require('jquery');
 	require('Common/Plugins/Widgets/jquery.ui.autocompleteselector');
-	require('BasicCTSSearch/Plugins/jquery.basicctsformtrack');
+	require('BasicCTSSearch/Plugins/jquery.basicctsformtrack')
 	var NCI = require('Common/Enhancements/NCI');
 
 
 	function _showCancerType() {
-		$("#fieldset-type").show()
-			.find('input').prop("disabled", false).removeClass("error").prev(".error-msg").hide();
-		$("#fieldset-keyword").hide()
-			.find('input').prop("disabled", true);
+		$("#fieldset-type").show();
+		$("#fieldset-type input").prop("disabled", false);
+		$("#fieldset-keyword").hide();
+		$("#fieldset-keyword input").prop("disabled", true);
 	}
 
 	function _showKeyword() {
-		$("#fieldset-type").hide()
-			.find('input').prop("disabled", true).removeClass("error").prev(".error-msg").hide();
-		$("#fieldset-keyword").show()
-			.find('input').prop("disabled", false);
+		$("#fieldset-keyword").show();
+		$("#fieldset-keyword input").prop("disabled", false);
+		$("#fieldset-type").hide();
+		$("#fieldset-type input").prop("disabled", true);
 	}
-
-	function _validateZip(val){
-		// This expression matches three different formats of postal codes: 5 digit US ZIP code, 5 digit US ZIP
-		// code + 4, and 6 digit alphanumeric Canadian Postal Code. The first one must be 5 numeric digits. The
-		// ZIP+4 must be 5 numeric digits, a hyphen, and then 4 numeric digits. The Canadian postal code must be
-		// of the form ANA NAN where A is any uppercase alphabetic character and N is a numeric digit from 0 to 9.
-		//^\d{5}-\d{4}|^\d{5}$|[A-Z]\d[A-Z] \d[A-Z]\d$
-		var pattern = /^\d{5}-\d{4}|^\d{5}$|[A-Z]\d[A-Z] \d[A-Z]\d$/;
-
-		return val.match(pattern)?true:false;
-	}
-
-	function _validateAge(val){
-		var pattern = /^\d{1,3}$/;
-		return val.match(pattern) && val < 120;
-	}
-
-	function _validateNotNull(val){
-		return val.length !== 0;
-	}
-
-	function _validateLocked(el){
-		if(!el.prop('disabled')) {
-			el.val("");
-		}
-		return el.prop('disabled');
-	}
-
-	function _toggleError(valid,el,skipTrue){
-		if(valid && !skipTrue){
-			el.removeClass("error");
-			el.prev('.error-msg').hide();
-		}
-		if(!valid){
-			el.addClass("error");
-			if(el.prev('.error-msg')[0]){
-				el.prev('.error-msg').show();
-			} else {
-				el.before('<div class="error-msg">' + el.data("error-message") + '</div>');
-			}
-		}
-	}
-
-
 
 
 	/**
@@ -74,73 +30,36 @@ define(function(require) {
 		$('.basiccts-keyword-toggle').click(function(){_showKeyword(); return false;});
 		_showCancerType(); //Start by hiding the keyword input.
 
-		$('#ct')
-			.autocompleteselector({
-				fetchSrc: '/BasicCTS.Service/v1/CancerTypeAutoSuggest',
-				queryParam: 'q'
-			})
-			.data('error-message','Please select a cancer type from the list.')
-			.on('blur.null',function(){
-				var $this = $(this);
-				_toggleError(_validateNotNull($this.val()),$this,true);
-				_toggleError(_validateLocked($this),$this,true);
-			})
-		;
-
-		$("#legend-keyword").next().find('input')
-			.prop('placeholder','Enter a keyword')
-		;
-
-
-		$("#legend-zip").next().find('input')
-			.data('error-message','Please enter a valid ZIP code')
-			.on('blur.error',function(){
-				var $this = $(this);
-
-				//validate zip format
-				_toggleError(_validateZip($this.val()),$this);
-			})
-		;
-
-		$('#legend-age').next().find('input')
-			.data('error-message','Please enter a valid age under 120')
-			.on('blur.error',function(){
-				var $this = $(this);
-
-				//validate age format
-				_toggleError(_validateAge($this.val()),$this);
-			})
-		;
+		$('#ct').autocompleteselector({
+			fetchSrc: '/BasicCTS.Service/v1/CancerTypeAutoSuggest',
+			queryParam: 'q'
+		});
 
 		//Wire Up Web Analytics
 		$(".clinical-trials-search-form").basicctsformtrack({
 			formName: 'clinicaltrials_basic'
-		}).submit(function(e) {
+		});
+
+		$(".clinical-trials-search-form").submit(function() {
 			//VALIDATE FIELDS!!!
-			e.preventDefault();
+			
 
-			//trigger input blur event
-			$(this).find('input[type=text]:visible').trigger('blur');
-
-			//check for errors again
-			var fieldsAreValid = $(this).find('input.error').length === 0;
-
-
+			var fieldsAreValid = true;
+			
 			if (fieldsAreValid) {
 				$(this).basicctsformtrack().completed();
-				$(this).submit();
 			} else {
 			// IF NOT VALID, call $(this).basicctsformtrack().errorMessages([
 			// 	{
 			// 		fieldid,
 			// 		errormessage
 			// 	}
-			// ]);
-				$(this).find('input.error:first').focus();
-
+			// ]);				
 				return false;
 			}
-		});
+			
+
+		})
 		
 	}
 
