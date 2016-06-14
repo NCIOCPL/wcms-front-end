27f08a7a71e4936d9997a23d3bb1914465e62a91
Frank Ali <alifr@users.noreply.github.com>
2016-06-14 21:15:06 +0000

    [OCEPROJECT-4127] Adding styles for home page CT delighter

---
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/Enhancements/clinicalTrialsDelighter.js
@@ -0,0 +1,7 @@
+define(function(require) {
+    var $ = require('jquery');
+
+    var delighter = $('<div id="delighter-homePage"><a href="/about-cancer/treatment/clinical-trials/basic">Find a <br/>Clinical Trial</a></div>');
+    delighter.insertAfter('#PageOptionsControl1');
+
+});
--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/HomePage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/HomePage.js
@@ -1,4 +1,5 @@
 define(function(require) {
 	require('Common/Enhancements/equal_heights');
 	require('Common/Enhancements/carousel');
+	require('Home/Enhancements/clinicalTrialsDelighter');
 });
