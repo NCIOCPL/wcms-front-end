4291acaff70d92408f79fe3a27ce5af775512c05
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-06-15 18:16:10 +0000

    Cleaning up trackOnPageLoad() function in ctsResultsAnalytics.js

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/Components/ctsResultsAnalytics.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/Components/ctsResultsAnalytics.js
@@ -5,15 +5,13 @@ var CTSResultsAnalytics = {
 	/**
 	* Track specified events and variables on page load
 	* @param count - total number of result items
-	* @param term - term or keyword used in search
-	* @param zip - zip code used in search
-	* @param age - age used in search
+	* @param fields - combination of term or keyword, zip, and/or age used in search
 	*/
-	trackOnPageLoad: function(count) {
+	trackOnPageLoad: function(count, fields) {
 		s.events='event2'; // Internal search event
 		s.prop10=count; // # of results
 		s.eVar11=s.prop11='clinicaltrials_basic'; // Search type
-		s.eVar22=s.prop22='term|zip|age'; // Search criteria
+		s.eVar22=s.prop22=fields; // Search parameters
 		s.t();
 	},
 	debug: function() {
