c28570c62dac2ed9a3b56fce8b1a31427f42966f
Dan Young <youngdr@users.noreply.github.com>
2016-06-16 03:05:19 +0000

    Updated page name from s_code.

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/Enhancements/clinicalTrialsDelighter.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/Enhancements/clinicalTrialsDelighter.js
@@ -1,10 +1,11 @@
 define(function(require) {
     var $ = require('jquery');
+	var AdobeAnalytics = require('Patches/AdobeAnalytics');
 
     var delighter = $('<div id="delighter-homePage"><a href="/about-cancer/treatment/clinical-trials/search">Find a <br/>Clinical Trial</a></div>');
     delighter.find('a').on('click.analytics',function(e){
-        var pageName = document.location.hostname + document.location.pathname;
-        NCIAnalytics.HomePageDelighterClick($(this), 'hp_find', pageName);
+		var s = AdobeAnalytics.getSObject();
+        NCIAnalytics.HomePageDelighterClick($(this), 'hp_find', s.pageName);
     });
     delighter.insertAfter('#PageOptionsControl1');
 
