25ebb4a15ff38526df933325ba77793c8e6ce47a
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-06-24 20:40:49 +0000

    //tracker.nci.nih.gov/browse/OCEPROJECT-4191 - Added email and print onclick analytics tracking - Moved popWindow JS snippet for email into initialize()

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/View/Enhancements/ctsViewAnalytics.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/View/Enhancements/ctsViewAnalytics.js
@@ -1,17 +1,43 @@
 define(function(require) {
 	var $ = require('jquery');
+	var AdobeAnalytics = require('Patches/AdobeAnalytics');
+
 
 	/***
 	* Main function
 	*/
 	function _initialize() {
-	/* Snippet to track clicks on accordion controls */
+		var s = AdobeAnalytics.getSObject();
+		var pageName = s.pageName;
+
+		/* Snippet to track clicks on accordion controls */
 		$('.accordion-controls').on('click.analytics', 'a', function (e) {
 			var $this = $(this);
 			id = 'clinical trial';
 			action = $this.attr('class');
 			NCIAnalytics.AccordionClick($this, id, action);
 		});
+		
+		/* Fire off analytics for print share click */
+		$('.cts-share a.print').on('click.analytics', function (e) {
+			var $this = $(this);
+			NCIAnalytics.PrintLink($this)
+			NCIAnalytics.SimpleCTSLink($this, 'print', pageName);
+		});
+		
+		/* Fire off analytics for Email share click */
+		$('.cts-share a.email').on('click.analytics', function (e) {
+			var $this = $(this);
+			var $href = $this.attr('href');
+			NCIAnalytics.eMailLink($this)
+			NCIAnalytics.SimpleCTSLink($this, 'email', pageName);
+			// Open email-share popup
+			if($href) {
+				dynPopWindow($this.attr('href'), 'emailPopUp', 'height=525,width=492'); 
+			}
+			return false;
+		});
+
   }
 
 	/**
