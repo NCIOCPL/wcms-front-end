b224f0783af1219d0e7a1978d78cb825be6b5f3d
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-07-07 19:49:24 +0000

    //tracker.nci.nih.gov/browse/WCMSFEQ-228 - added breadcrumb hack to hide first level

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.Microsites.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.Microsites.js
@@ -15,9 +15,16 @@ define(function(require) {
 			
 			//Add the 'micro-a' class to the guide card row
 			$('.row.guide-card').addClass('micro-a');
-			
+
+			/** TODO: Remove these hacks once permanent fixes are in place */
 			//Do not show the delighter
 			$('#delighter-homePage').css('display','none'); 
+			// Do not show the CancerGov 'Home' link in the breadcrumb
+			// Permanent fix will be in CDE code
+			if($('.breadcrumbs li a').first().attr('href') == '/') {
+				$('.breadcrumbs li').first().remove();
+			}
+			/** End hacks */
 		}
 	}
 
