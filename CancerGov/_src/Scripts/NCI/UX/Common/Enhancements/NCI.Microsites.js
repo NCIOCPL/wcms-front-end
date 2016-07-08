cb2dd374607be86b5bb4dafd1b4ee17bfa0c2bec
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-07-08 15:52:42 +0000

    //tracker.nci.nih.gov/browse/WCMSFEQ-228 - Cleaned up logic in _initialize() function

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.Microsites.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.Microsites.js
@@ -5,25 +5,33 @@ define(function(require) {
 	* Main function
 	*/
 	function _initialize() {
-		var url = document.URL;
+		var micrositeId = $('header #nvcgSlSiteBanner').html();
 
-		//Set styles for OCNR microsite only
-		if(url.indexOf('/doc/ocnr') > -1) {
-			
-			//Add the 'micro-a' class to various elements 
-			$('#nvcgSlUtilityBar').addClass('micro-a'); // Utility nav
-			$('.language-bar').addClass('micro-a'); // Language bar (for mobile)
-			$('.feature-primary-title').addClass('micro-a'); // Feature card title (for mobile)
-			$('.row.guide-card').addClass('micro-a'); // Guide card row
+		/* Set styles for OCNR microsite only
+		* This checks for a specific logo name in order to fire off the microsite-specific Javascript
+		* and styling. This may not be the best way to do it, but it's safer than trying to
+		* match a pattern in the URL. 
+		* Ideally, we can put a unique id or class name in the header Raw HTML and kick off the Javascript
+		* based on that.
+		*/
+		if(micrositeId) {
+			if(micrositeId.indexOf('ocnr-logo') > -1) {
 
-			/** TODO: Remove these hacks once permanent fixes are in place */
-			//Do not show the delighter
-			$('#delighter-homePage').css('display','none'); 
-			// Do not show the CancerGov 'Home' link in the breadcrumb
-			// Permanent fix will be in CDE code
-			if($('.breadcrumbs li a').first().attr('href') == '/') {
-				$('.breadcrumbs li').first().remove();
-			} /** End hacks */
+				//Add the 'micro-a' class to selected elements 
+				$('#nvcgSlUtilityBar').addClass('micro-a'); // Utility nav
+				$('.language-bar').addClass('micro-a'); // Language bar (for mobile)
+				$('.feature-primary-title').addClass('micro-a'); // Feature card title (for mobile)
+				$('.row.guide-card').addClass('micro-a'); // Guide card row
+
+				/** TODO: Remove these hacks once permanent fixes are in place */
+				// Do not show the delighter
+				$('#delighter-homePage').css('display','none'); 
+				// Do not show the CancerGov 'Home' link in the breadcrumb
+				// Permanent fix will be in CDE code
+				if($('.breadcrumbs li a').first().attr('href') == '/') {
+					$('.breadcrumbs li').first().remove();
+				} /** End hacks */
+			}
 		}
 	}
 
