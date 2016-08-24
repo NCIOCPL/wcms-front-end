b36be090fefba61e7907c7b6410fbef6782dd8c8
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-08-24 17:07:57 +0000

    //tracker.nci.nih.gov/browse/OCECTS-245 - added tracking to cancel and submit clicks in the feedback form, with the caveat that the elements can still change

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Common/Enhancements/FeedbackForm.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Common/Enhancements/FeedbackForm.js
@@ -199,7 +199,7 @@ define(function(require){
                 href: "#"
             }).on('click', function(){
                 _openFeedbackForm($feedbackDelighter);                
-				_initializeFormOpenAnalytics();
+				_initializeFeedbackFormAnalytics();
                 return false;
             }).prependTo($delighterContainer)
             .append(
@@ -210,17 +210,36 @@ define(function(require){
                     '<p>Help us make clinical trials searching better</p>'    
                 )
             ); 
-
         }
     }
     
-    // Track feedback form opening on right rail
-    function _initializeFormOpenAnalytics(){
+    // Initialize Feedback Form analytics tracking upon opening of the form
+	// Tracked events are passed to FeedbackFormClick() in NCIAnalyticsFunctions.js
+	//  1) Track clicks on the Right Rail to open the feedback form
+	//  2) Track "cancel" button click in the form (event is bound to the form's button element containing "Close" as inner text)
+	//  3) Track "submit" button click in the form (event is bound to the form's button element with the "submit" class attribute)
+	// - TODO: Update jQuery elements as needed once design is finalized
+    function _initializeFeedbackFormAnalytics(){
         if(!!NCIAnalytics && !!NCIAnalytics.FeedbackFormClick) {
+            // 1) Track feedback form opening on right rail
             NCIAnalytics.FeedbackFormClick(this, 'rrail_send us your feedback');
+			
+            // 2) Track cancel/close events inside the feedback form
+            $(".cts-feedback-dialog button:contains('Close')").each(function(i,el){
+                $(el).click(function(e){
+                    NCIAnalytics.FeedbackFormClick(this, 'cts_basic_feedback - dismiss');
+                });
+            });
+
+            // 3) Track submit events inside the feedback form
+            $(".cts-feedback-dialog button.submit.button").each(function(i,el){
+                $(el).click(function(e){
+                    NCIAnalytics.FeedbackFormClick(this, 'cts_basic_feedback - submit');
+                });
+            });
         }
     }
-
+	
     /* Flag for telling whether this enhancement has been initialized. */
 	var _initialized = false;
 	
