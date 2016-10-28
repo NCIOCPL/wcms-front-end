af29af8de68bf2db89c499295a354949d322af76
Ben Ferry <ferrybl-nih@users.noreply.github.com>
2016-10-28 18:39:37 +0000

    [OCEPROJECT-4280][OCEPROJECT-4281][OCEPROJECT-4283] - Added template for Blog Post pages and Blog Series pages.

---
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/BlogPost/BlogPostPage.js
@@ -0,0 +1,28 @@
+define(function(require) {
+	require('Common/Enhancements/sharecomponent');
+	$(function() {
+		require('Common/Enhancements/analytics.After').init();
+	});
+
+	// Ensure the .contentzone is given a width of 75% making room for the right rail when present
+	var setContentWidth = function(){
+		if($('#nvcgSlListBlogRTRail').length){
+			if ($(window).width() >= 1025){
+            	$('.contentzone').css('width', '75%');
+                $("#PageOptionsControl1").appendTo("#blogPageOptionsOuterContainer");
+			}
+			else{
+				$('.contentzone').css('width', '100%');
+                $("#PageOptionsControl1").appendTo("#blogPageOptionsInnerContainer");
+			}
+        }
+	};
+	$(window).resize(function(){
+		setContentWidth();
+	});
+    $( document ).ready(function() {
+        setContentWidth();
+    });
+});
+
+
