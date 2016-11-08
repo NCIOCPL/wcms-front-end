052ace420a0bd5c9ad7225f68e646e22799c7c21
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-11-08 19:47:56 +0000

    //tracker.nci.nih.gov/browse/OCECTS-280 - Hid delighter on CT search, results, and view pages

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/clinicalTrialsDelighter.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/clinicalTrialsDelighter.js
@@ -2,15 +2,17 @@ define(function(require) {
     var $ = require('jquery');
     var AdobeAnalytics = require('Patches/AdobeAnalytics');
 
-    /* Only display the delighter on the NCI Home page but not on other
-     * pages regardless if they are using the home page template
+    /* Display the delighter on all pages under the /clinical-trials path, except for the search/results/view pages
+     * TODO: add this as a require for landing pages as well
      * ---------------------------------------------------------------- */
-    if( location.pathname.indexOf("about-cancer/treatment/clinical-trials/") > -1 ) {
-        var delighter = $('<div id="delighter-innerpage"><a href="/about-cancer/treatment/clinical-trials/search">Find a <br/>Clinical Trial</a></div>');
-        delighter.find('a').on('click.analytics',function(e){
-            var s = AdobeAnalytics.getSObject();
-            NCIAnalytics.HomePageDelighterClick($(this), 'hp_find', s.pageName);
-        });
-        delighter.insertAfter('#PageOptionsControl1');
+    if( location.pathname.indexOf("about-cancer/treatment/clinical-trials") > -1 ) {
+        if( location.pathname.indexOf("/clinical-trials/search") == -1 ) {
+            var delighter = $('<div id="delighter-innerpage"><a href="/about-cancer/treatment/clinical-trials/search">Find a <br/>Clinical Trial</a></div>');
+            delighter.find('a').on('click.analytics',function(e){
+                var s = AdobeAnalytics.getSObject();
+                NCIAnalytics.HomePageDelighterClick($(this), 'hp_find', s.pageName);
+            });
+            delighter.insertAfter('#PageOptionsControl1');
+        }
     }
 });
