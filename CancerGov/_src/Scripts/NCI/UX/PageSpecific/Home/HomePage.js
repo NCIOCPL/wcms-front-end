17a473e65ddcc06b699dfd71852df579091b788d
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-11-09 18:06:46 +0000

    //tracker.nci.nih.gov/browse/OCECTS-280 - added floating delighter to other "clinical-trials/" pages

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/HomePage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Home/HomePage.js
@@ -1,5 +1,5 @@
 define(function(require) {
 	require('Common/Enhancements/equal_heights');
 	require('Common/Enhancements/carousel');
-	require('Home/Enhancements/clinicalTrialsDelighter');
+	require('Common/Enhancements/clinicalTrialsDelighter');
 });
--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/InnerPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/InnerPage.js
@@ -1,6 +1,6 @@
 define(function(require) {
 	require('Common/Enhancements/sharecomponent');
-	require('Inner/Enhancements/clinicalTrialsDelighter');
+	require('Common/Enhancements/clinicalTrialsDelighter');
 	$(function() {
 		require('Common/Enhancements/analytics.After').init();
 	});
