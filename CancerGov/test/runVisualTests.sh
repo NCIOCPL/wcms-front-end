ee067ced46582c917b7cc5cad665758d6280157e
Volker Englisch <venglisch@users.noreply.github.com>
2016-11-29 23:37:34 +0000

    Adding a help (-h) option

--- CancerGov/test/runVisualTests.sh
+++ CancerGov/test/runVisualTests.sh
@@ -3,13 +3,28 @@
 echo "Running Tests"
 
 set -o errexit # Exit on error
+
+usage="$(basename "$0") [-h] [-b] [-f filename]
+
+where:
+    -h  show this help message
+    -b  run Phantom to create a baseline for all files (default: www.cancer.gov)
+    -f  run Phantom for a single file and compare to baseline (default: localhost)
+    no option
+        run Phantom for all files and compare to baseline
+"
+
 CASPER_PATH='../node_modules/casperjs'
 CASPER_BIN=$CASPER_PATH'/bin'
 PHANTOMJS='../node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs'
 SERVER='dev'
 FILE=''
-while getopts "bf:" opt; do
-  case $opt in
+while getopts "bf:h" opt; do
+  case "$opt" in
+    h)
+      echo "$usage"
+      exit
+      ;;
     b)
       echo "Running baseline tests on production server" >&2
       SERVER='prod'
