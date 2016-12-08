88673beb8de6d3b3090ff95adc13441cc1ab5f52
Volker Englisch <venglisch@users.noreply.github.com>
2016-12-08 23:04:55 +0000

    Checking in for FEQ build

--- CancerGov/test/runVisualTests.bat
+++ CancerGov/test/runVisualTests.bat
@@ -6,7 +6,6 @@ set CASPER_PATH=%~dp0node_modules\casperjs
 set CASPER_BIN=%CASPER_PATH%\bin\
 set PHANTOMJS=%~dp0node_modules\phantomjs-prebuilt\lib\phantom\bin\phantomjs.exe
 
-rem call "%PHANTOMJS%" "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli test tests/%1 --includes=initialize.js,visualTest.js --post=cleanup.js
 
 SET argA=default for A
 SET argB=default for B
@@ -14,12 +13,19 @@ SET server=dev
 SET file=XXX
 
 :loop
-IF [%1] == [] GOTO continue
-IF [%1] == [/h] GOTO help
-if [%1] == [/B] GOTO baseline
-if [%1] == [/b] GOTO baseline
-if [%1] == [/F] GOTO filecheck
-if [%1] == [/f] GOTO filecheck
+IF [%1] == [] GOTO continue 
+IF [%1] == [/h] GOTO help 
+IF [%1] == [/B] GOTO baseline 
+IF [%1] == [/b] GOTO baseline 
+IF [%1] == [/F] GOTO filecheck 
+IF [%1] == [/f] ( 
+    GOTO filecheck 
+) ELSE (
+   ECHO 
+   ECHO Invalid option!
+   ECHO
+   GOTO help
+)
 
 echo "DADA"
 echo "DUDU"
@@ -35,8 +41,8 @@ SET server=prod
 
 IF EXIST "tests\%~n2.js" (
    SET file=%~n2.js
-) ELSE IF EXIST "tests\%~n2" (
-   SET file=%~n2
+) ELSE ( 
+   IF EXIST "tests\%~n2" SET file=%~n2
 )
 echo file name: %file%
 echo file name: tests\%file%
@@ -79,7 +85,9 @@ GOTO continue
 
 :run
 ECHO PHANTOMJS %CASPER_BIN%bootstrap.js --casper-path=%CASPER_PATH% --cli test tests/%file% --includes=initialize.js,visualTest.js --post=cleanup.js --server=%server%
-PHANTOMJS %CASPER_BIN%bootstrap.js --casper-path=%CASPER_PATH% --cli test tests\%file% --includes=initialize.js,visualTest.js --post=cleanup.js --server=%server%
+
+ECHO %PHANTOMJS%
+call "%PHANTOMJS%" "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli test tests/%1 --includes=initialize.js,visualTest.js --post=cleanup.js
 
 
 :continue
