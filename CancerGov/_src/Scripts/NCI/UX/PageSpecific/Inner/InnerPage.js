74b4b5dd0ae84652522d69c133912aa05c68aa66
Frank Ali <alifr@users.noreply.github.com>
2017-03-23 19:25:27 +0000

    Restoring CTS Delighters to other CTS subpages

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/InnerPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/InnerPage.js
@@ -1,8 +1,8 @@
 define(function(require) {
 	require('Common/Enhancements/sharecomponent');
-	require('Common/Enhancements/clinicalTrialsDelighter');
 	require('Inner/Enhancements/showHideListingBodyField');
 	$(function() {
+		require('Common/Enhancements/clinicalTrialsDelighter');
 		require('Common/Enhancements/analytics.After').init();
 	});
 });
--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Landing/LandingPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Landing/LandingPage.js
@@ -1,9 +1,10 @@
 define(function(require) {
 	require('Modules/carousel/carousel');
-	require('Common/Enhancements/clinicalTrialsDelighter');
 	require('Plugins/jquery.nci.equal_heights');
 
 	$(function() {
+		require('Common/Enhancements/clinicalTrialsDelighter');
+
 		$('[data-match-height]').NCI_equal_heights();
 	});
 });
