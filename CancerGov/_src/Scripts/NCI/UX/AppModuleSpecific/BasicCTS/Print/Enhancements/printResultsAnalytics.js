c05bb566085175bb3634aff11e113798e1a75594
Ben Ferry <ferrybl-nih@users.noreply.github.com>
2017-03-27 21:15:25 +0000

    [OCECTS-358] - Add analytics for "check for updates" link on print page.

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Print/Enhancements/printResultsAnalytics.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Print/Enhancements/printResultsAnalytics.js
@@ -5,22 +5,45 @@ define(function(require) {
 	* Main function
 	*/
 	function _initialize() {
+		var printPageID = getPrintID;
+
+		// Loop through all the "Check for Update" buttons and assign them unique IDs.
+		var counter = 1;
+		$('.CheckForUpdatesButton').each(function(){
+			$(this).attr('id', counter);
+			counter ++;
+		});
+		
 		$('#printPage').click(function(e){
 			var $this = $(this);
-			NCIAnalytics.ClinicalTrialsPrintResults_TopLinkClick($this, 'printPage');
+			NCIAnalytics.CTSPrintResults_TopLinkClick($this, 'Print Page', printPageID);
 		});
 
 		$('#ctl10_EmailResults').click(function(e){
 			var $this = $(this);
-			NCIAnalytics.ClinicalTrialsPrintResults_TopLinkClick($this, 'email');
+			NCIAnalytics.CTSPrintResults_TopLinkClick($this, 'Email', printPageID);
 		});
 
 		$('#newSearch').click(function(e){
 			var $this = $(this);
-			NCIAnalytics.ClinicalTrialsPrintResults_TopLinkClick($this, 'newSearch');
+			NCIAnalytics.CTSPrintResults_TopLinkClick($this, 'New Search', printPageID);
 		});
+
+		$('.CheckForUpdatesButton').click(function(e){
+			var $this = $(this);
+			NCIAnalytics.CTSPrintResults_VewUpdatesLinkClick($this, printPageID);
+		});		
+
 	}
 
+	function getPrintID(){
+		var url = window.location.href;
+		var regex = new RegExp("[?&]" + "PrintID" + "(=([^&#]*)|&|#|$)"),
+        var results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 
 	/**
 	 * Identifies if this enhancement has been initialized or not.
