13181443aed4eeeaeb52679fc5273b9fc3cf67eb
Frank Ali <alifr@users.noreply.github.com>
2017-05-09 19:28:34 +0000

    [WCMSFEQ-631] Fixing font resize module.

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.PageOptions.FontResizer.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.PageOptions.FontResizer.js
@@ -9,24 +9,21 @@ define(function(require) {
         multiplier: 1.2,
         init: function() {
             this.originalSize = parseFloat($("body").css("font-size"), 10);
-            $(this.selector).click(this.click);
             this.$content = $(".resize-content");
+            $(this.selector).click(this.click);
         },
         click: function(e) {
-            var self = NCI.PageOptions.FontResizer; // this is the target in click events.
             e.preventDefault(); // no event bubbling.
-            self.setCurrentFontSize();
+            var self = FontResizer; // this is the target in click events.
+            self.currentSize = parseFloat(self.$content.css("font-size"), 10);
             var newSize = self.currentSize * self.multiplier;
             newSize = newSize > 30 ? self.originalSize : newSize;
-            $content.css("font-size", newSize + "px");
-            if (typeof equalHeights === typeof Function.prototype) {
-                equalHeights();
-            }
+            self.$content.css("font-size", newSize + "px");
+            //TODO: trigger a font-resize event for subscribers such as equalHeights
+            // if (typeof equalHeights === typeof Function.prototype) {
+            //     equalHeights();
+            // }
             return false; //technically unnecessary to return false.
-        },
-        setCurrentFontSize: function() {
-            $content = $(".resize-content");
-            this.currentSize = parseFloat($content.css("font-size"), 10);
         }
     };
 
