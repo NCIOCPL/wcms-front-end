bdc618566322f04e2b65cf56543791b735365569
Meisha Thompson <mltnewman@users.noreply.github.com>
2017-06-01 18:58:16 +0000

    [WCMSFEQ-653] - Modularize content page - video embedding

--- CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
@@ -8,6 +8,7 @@ define(function(require) {
 	var backToTop = require('Modules/backToTop/backToTop');
 	var NCIAccordion = require('Modules/accordion/accordion');
 	var tableToggle = require('Modules/tableToggle/tableToggle');
+	var flexVideo = require('Modules/videoPlayer/flexVideo');
 
 
 	//require('Modules/autocomplete/autocomplete');
@@ -17,7 +18,7 @@ define(function(require) {
 
 
 	jQuery(document).ready(function(jQuery) {
-		
+
 		/*** BEGIN dictionary toggle ***/
 		(function($) {
 			var dictionaryToggle = function() {
@@ -54,7 +55,7 @@ define(function(require) {
 			exitDisclaimer.init();
 		})();
 		/*** END Exit Disclaimer ***/
-		
+
 		/*** BEGIN Page Options
 		 * This functions the font resizer.
 		 ***/
@@ -79,60 +80,7 @@ define(function(require) {
 		 * This enables the embedding of YouTube videos and playlists as iframes.
 		 ***/
 		(function($) {
-			$('.flex-video').each(function() {
-				var $this = $(this);
-				var lang = $('html').attr('lang') || 'en';
-				var contentLanguage = document.documentElement.lang;
-				var hl = '&hl=en';
-				if(contentLanguage.indexOf('es') > -1){
-					hl = '&hl=es';
-				}
-
-				var videoSrc = '//www.youtube-nocookie.com/embed/',
-					videoLinkSrc = 'https://www.youtube.com/',
-					videoId = '',
-					videoTitle = '',
-					videoOptions = '?wmode=opaque&rel=0' + hl,
-					videoType = '';
-
-				if($this.hasClass('playlist')) {
-					videoType = 'playlist';
-					videoTitle = $this.attr('data-playlist-title');
-					videoId = 'videoseries';
-					videoOptions = videoOptions +
-						'&list=' + $this.attr('data-playlist-id');
-					videoLinkSrc = videoLinkSrc + 'playlist?list=' + videoId;
-				} else {
-					videoType = 'video';
-					videoTitle = $this.attr('data-video-title');
-					videoId = $this.attr('data-video-id');
-					videoOptions = videoOptions +
-						'';
-					videoLinkSrc = videoLinkSrc + 'watch?v=' + videoId;
-				}
-				videoSrc = videoSrc + videoId + videoOptions;
-				var videoText = {
-					video: {
-						en: 'Youtube embedded video: ' + videoLinkSrc,
-						es: 'Video insertado desde YouTube: ' + videoLinkSrc
-					},
-					playlist: {
-						en: 'Youtube embedded video playlist: ' + videoLinkSrc,
-						es: 'Lista de reproducci&oacute;n insertada desde YouTube: ' + videoLinkSrc
-					}
-				};
-				$this.append(
-					$(document.createElement('iframe'))
-						.attr('width', '560')
-						.attr('height', '315')
-						.attr('src', videoSrc)
-						.attr('frameborder', '0')
-						.attr('allowFullScreen', '')
-						.attr('title', videoTitle)
-						.attr('alt', videoTitle)
-						.text(videoText[videoType][lang])
-				);
-			});
+			flexVideo.init();
 		})(jQuery);
 		/*** END video embedding ***/
 
@@ -335,20 +283,20 @@ define(function(require) {
 		})(jQuery);
 		/*** END blog comment policy ***/
 
-		/*** BEGIN HACK for Blog Series titles 
-		* TODO: remove when Blog Dynamic List Percussion template is updated 
+		/*** BEGIN HACK for Blog Series titles
+		* TODO: remove when Blog Dynamic List Percussion template is updated
 		* with class name for <h3> ***/
 		(function($) {
 			$('div.blog-post').each(function () {
-				if ($(this).find('a.comment-count').length < 1) {	
-					($(this).find('div.post-title h3').addClass('no-comments'))	
+				if ($(this).find('a.comment-count').length < 1) {
+					($(this).find('div.post-title h3').addClass('no-comments'))
 				}
 			});
 		})(jQuery);
 		/*** END HACK for Blog Series titles ***/
 
 		// For the new reference tool tip handling uncomment the following:
-		//require("Common/Enhancements/NCI.ReferenceToolTip").init(); 
+		//require("Common/Enhancements/NCI.ReferenceToolTip").init();
 
 		//Then uncomment the reference tooltips function below:
 		// reference tooltips
@@ -466,7 +414,7 @@ define(function(require) {
 				});
 		})(jQuery);
 		//END REFERENCE TOOL TIPS
-		
+
 		// initialize the prevent-enter enhancement
 		(function ($) {
 			$('[data-prevent-enter="true"]').NCI_prevent_enter();
@@ -536,10 +484,10 @@ define(function(require) {
 			endDate:'04/01/2017'
 		});
 
-		
+
 		// Blue Ribbon Panel - Page Specific
 		require("Common/Enhancements/NCI.Video").init();
-		
+
 
 	});
 
