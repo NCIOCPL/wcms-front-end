e87cba1a3fd056943f97621f8f073be03314590d
Frank Ali <alifr@users.noreply.github.com>
2017-06-02 13:36:59 +0000

    Cleaning up function encapsulation

--- CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/ContentPage.js
@@ -1,4 +1,6 @@
 define(function(require) {
+
+    var $ = require('jquery');
 	var $script = require('scriptjs');
     require('Common/Enhancements/popup_functions');
 	//require('scrolltofixed');
@@ -18,16 +20,16 @@ define(function(require) {
 	//require('fork-placeholders.js');
 
 
-	jQuery(document).ready(function(jQuery) {
+	$(document).ready(function() {
 
 		/*** BEGIN dictionary toggle ***/
-		(function($) {
+		(function() {
 			var dictionaryToggle = function() {
 				$("#utility-dropdown").slideToggle(0, function () {
 					$("#utility-dictionary").toggleClass('active');
 				});
 			}
-		})(jQuery);
+		})();
 		/*** END dictionary togle ***/
 
 		/*** BEGIN back-to-top ***/
@@ -37,7 +39,7 @@ define(function(require) {
 		/*** END back-to-top ***/
 
 		/*** BEGIN mobile nav ("off-canvas flyout functionality") ***/
-		(function($) {
+		(function() {
 			// OCEPROJECT-3098 HACK to fix the Spanish mega menu on the Spanish homepage
 			if(/^\/espanol\/?$/.test(location.pathname)) {
 				$('#mega-nav .contains-current').removeClass('contains-current');
@@ -46,7 +48,7 @@ define(function(require) {
 			NCI.Nav.init();
 
 			NCI.Search.init();
-		})(jQuery);
+		})();
 		/*** END mobile nav ***/
 
 		/*** BEGIN Exit Disclaimer
@@ -61,9 +63,9 @@ define(function(require) {
 		 * This functions the font resizer.
 		 ***/
 		 // MOVED TO NCI.PageOptions.FontResizer.js
-		(function($) {
+		(function() {
 			NCI.PageOptions.init();
-		})(jQuery);
+		})();
 		/*** END Page Options **/
 
 		/*** BEGIN table toggling
@@ -71,22 +73,22 @@ define(function(require) {
 		 * An example can be seen on grants-research-funding.shtml,
 		 * as of the first commit with this code.
 		 ***/
-		(function($) {
+		(function() {
 			// for each toggleable section...
 			tableToggle.init();
-		})(jQuery);
+		})();
 		/*** END table toggling ***/
 
 		/*** BEGIN video embedding
 		 * This enables the embedding of YouTube videos and playlists as iframes.
 		 ***/
-		(function($) {
+		(function() {
 			flexVideo.init();
-		})(jQuery);
+		})();
 		/*** END video embedding ***/
 
 		/*** BEGIN form controls ***/
-		(function($) {
+		(function() {
 
 			$.ui.selectmenu.prototype._buttonEvents.keydown = function( event ) {
 				var preventDefault = true;
@@ -242,17 +244,17 @@ define(function(require) {
 					width: $this.hasClass('fullwidth') ? '100%' : null
 				}).selectmenu('menuWidget').addClass('scrollable-y');
 			});
-		})(jQuery);
+		})();
 		/*** END form controls ***/
 
 		/*** BEGIN accordionizer ***/
-		(function($) {
+		(function() {
 			NCIAccordion.makeAllAccordions();
-		})(jQuery);
+		})();
 		/*** END accordionizer ***/
 
 		/*** BEGIN page outlining ***/
-		(function($) {
+		(function() {
 			// generate the page outline -- this is used for all page-/document-level navigation
 			// set up outlines
 			$('article').each(function() {
@@ -271,130 +273,132 @@ define(function(require) {
 			if ($('article').length > 0) {
 				NCI.buildOTP();
 			}
-		})(jQuery);
+		})();
 		/*** END page outlining ***/
 
 		/*** BEGIN blog comment policy ***/
-		(function($) {
+		(function() {
 			if ($('#cgvCommentsSl').length) {
 				if( $('.intense-debate-comments').length < 1) {
 					$('.blog-comment-policy').show();
 				}
 			}
-		})(jQuery);
+		})();
 		/*** END blog comment policy ***/
 
 		/*** BEGIN HACK for Blog Series titles
 		* TODO: remove when Blog Dynamic List Percussion template is updated
 		* with class name for <h3> ***/
-		(function($) {
+		(function() {
 			$('div.blog-post').each(function () {
 				if ($(this).find('a.comment-count').length < 1) {
 					($(this).find('div.post-title h3').addClass('no-comments'))
 				}
 			});
-		})(jQuery);
+		})();
 		/*** END HACK for Blog Series titles ***/
 
 		// reference tooltips
-		(function ($) {
+		(function () {
 			tooltips.init();
-		})(jQuery);
+		})();
 		//END REFERENCE TOOL TIPS
 
 		// initialize the prevent-enter enhancement
-		(function ($) {
+		(function () {
 			$('[data-prevent-enter="true"]').NCI_prevent_enter();
-		})(jQuery);
+		})();
 
 
-		// Proactive Live Help for CTS
-		var ProactiveLiveHelp = require("Modules/proactiveLiveHelp/proactiveLiveHelp");
+        // Proactive Live Help for CTS
+        (function () {
+			var ProactiveLiveHelp = require("Modules/proactiveLiveHelp/proactiveLiveHelp");
 
-		var ProactiveLiveHelpforCTS = new ProactiveLiveHelp();
-		var ProactiveLiveHelpforColorectal = new ProactiveLiveHelp();
+			var ProactiveLiveHelpforCTS = new ProactiveLiveHelp();
+			var ProactiveLiveHelpforColorectal = new ProactiveLiveHelp();
 
-		//console.log(ProactiveLiveHelp);
+			//console.log(ProactiveLiveHelp);
 
-		ProactiveLiveHelpforCTS.initialize({
-			urls: [
-				"/about-cancer/treatment/clinical-trials/search",
-				"/about-cancer/treatment/clinical-trials/basic",
-				"/about-cancer/treatment/clinical-trials/search/r",
-				"/about-cancer/treatment/clinical-trials/search/v",
-				"/about-cancer/treatment/clinical-trials/advanced-search",
-				"/about-cancer/treatment/clinical-trials/search/results",
-				"/about-cancer/treatment/clinical-trials/search/view"
-			],
-			popupID: 'ProactiveLiveHelpForCTSPrompt',
-			popupTitle: "Questions about Clinical Trials?",
-			optOutDurationDays: 30,
-			popupDelaySeconds: 90
-		});
-		// END Clinical Trial Search Setup
+			ProactiveLiveHelpforCTS.initialize({
+				urls: [
+					"/about-cancer/treatment/clinical-trials/search",
+					"/about-cancer/treatment/clinical-trials/basic",
+					"/about-cancer/treatment/clinical-trials/search/r",
+					"/about-cancer/treatment/clinical-trials/search/v",
+					"/about-cancer/treatment/clinical-trials/advanced-search",
+					"/about-cancer/treatment/clinical-trials/search/results",
+					"/about-cancer/treatment/clinical-trials/search/view"
+				],
+				popupID: 'ProactiveLiveHelpForCTSPrompt',
+				popupTitle: "Questions about Clinical Trials?",
+				optOutDurationDays: 30,
+				popupDelaySeconds: 90
+			});
+			// END Clinical Trial Search Setup
 
-		// Proactive Live Help for Colo-rectal Cancer Type Pages
-		ProactiveLiveHelpforColorectal.initialize({
-			urls: [
-				'/types/colorectal',
-				'/types/colorectal/patient/colon-treatment-pdq',
-				'/types/colorectal/patient/rectal-treatment-pdq',
-				'/types/colorectal/patient/colorectal-prevention-pdq',
-				'/types/colorectal/patient/colorectal-screening-pdq',
-				'/types/colorectal/hp',
-				'/types/colorectal/hp/colon-treatment-pdq',
-				'/types/colorectal/hp/rectal-treatment-pdq',
-				'/types/colorectal/hp/colorectal-prevention-pdq',
-				'/types/colorectal/hp/colorectal-genetics-pdq',
-				'/types/colorectal/hp/colorectal-screening-pdq',
-				'/types/colorectal/research',
-				'/types/colorectal/screening-fact-sheet',
-				'/types/colorectal/did-you-know-colorectal-cancer-screening-video',
-				'/types/colorectal/research/cetuximab-chemo-no-benefit',
-				'/types/colorectal/research/colonoscopy-reduces-deaths',
-				'/types/colorectal/research/eflornithine-sulindac',
-				'/types/colorectal/research/folfox-celecoxib',
-				'/types/colorectal/research/preop-treatment',
-				'/types/colorectal/research/screening-sigmoidoscopy',
-				'/types/colorectal/research/aspirin-reduces-risk',
-				'/types/colorectal/research/TAS-102-overall-survival',
-				'/types/colorectal/research/bevacizumab-severe-side-effects',
-				'/types/colorectal/research/virtual-colonoscopy-results-qa',
-				'/types/colorectal/research/polyp-fiber-prevention-qa',
-				'/about-cancer/treatment/drugs/colorectal'
-			],
-			popupID: 'PLH-colorectal',
-			popupTitle: 'Questions about Colorectal Cancer?',
-			optOutDurationDays: 14,
-			popupDelaySeconds: 30,
-            start:'03/01/2017',
-			endDate:'04/01/2017'
-		});
+			// Proactive Live Help for Colo-rectal Cancer Type Pages
+			ProactiveLiveHelpforColorectal.initialize({
+				urls: [
+					'/types/colorectal',
+					'/types/colorectal/patient/colon-treatment-pdq',
+					'/types/colorectal/patient/rectal-treatment-pdq',
+					'/types/colorectal/patient/colorectal-prevention-pdq',
+					'/types/colorectal/patient/colorectal-screening-pdq',
+					'/types/colorectal/hp',
+					'/types/colorectal/hp/colon-treatment-pdq',
+					'/types/colorectal/hp/rectal-treatment-pdq',
+					'/types/colorectal/hp/colorectal-prevention-pdq',
+					'/types/colorectal/hp/colorectal-genetics-pdq',
+					'/types/colorectal/hp/colorectal-screening-pdq',
+					'/types/colorectal/research',
+					'/types/colorectal/screening-fact-sheet',
+					'/types/colorectal/did-you-know-colorectal-cancer-screening-video',
+					'/types/colorectal/research/cetuximab-chemo-no-benefit',
+					'/types/colorectal/research/colonoscopy-reduces-deaths',
+					'/types/colorectal/research/eflornithine-sulindac',
+					'/types/colorectal/research/folfox-celecoxib',
+					'/types/colorectal/research/preop-treatment',
+					'/types/colorectal/research/screening-sigmoidoscopy',
+					'/types/colorectal/research/aspirin-reduces-risk',
+					'/types/colorectal/research/TAS-102-overall-survival',
+					'/types/colorectal/research/bevacizumab-severe-side-effects',
+					'/types/colorectal/research/virtual-colonoscopy-results-qa',
+					'/types/colorectal/research/polyp-fiber-prevention-qa',
+					'/about-cancer/treatment/drugs/colorectal'
+				],
+				popupID: 'PLH-colorectal',
+				popupTitle: 'Questions about Colorectal Cancer?',
+				optOutDurationDays: 14,
+				popupDelaySeconds: 30,
+				start:'03/01/2017',
+				endDate:'04/01/2017'
+			});
+        })();
 
 
 		// Blue Ribbon Panel - Page Specific
 		require("Common/Enhancements/NCI.Video").init();
 
+        // BEGIN Spanish Analytics tracking
+        $('html[lang="es"]').find('a.news-govdelivery, a.blogRSS').on('click', function() {
+            s.linkTrackVars = 'prop4,prop5';
+            s.prop4 = 'GovDeliveryEsp';
+            s.prop5 = 'www.cancer.gov' + location.pathname.toLowerCase();
+            s.tl(this, 'o', 'GovDeliveryEsp');
+        });
+        // END Spanish Analytics Tracking
 
 	});
 
 	// BEGIN Table Resizing
-	jQuery(window).on('load', function () {
+	$(window).on('load', function () {
 		//Table enlarging & scrollbar adding.
 		//This marks all tables as scrollable, but only adds a shadow to the right side if it is scrolling.
 		//Inspired by http://www.456bereastreet.com/archive/201309/responsive_scrollable_tables/
-		(function ($) {
+		(function () {
 			$("#content table").overflowEnlarge();
-		})(jQuery);
+		})();
 	});
 	// END Table Resizing
 
-	// BEGIN Spanish Analytics tracking
-	$('html[lang="es"]').find('a.news-govdelivery, a.blogRSS').on('click', function() {
-	    s.linkTrackVars = 'prop4,prop5';
-	    s.prop4 = 'GovDeliveryEsp';
-	    s.prop5 = 'www.cancer.gov' + location.pathname.toLowerCase();
-	    s.tl(this, 'o', 'GovDeliveryEsp');
-	});
-	// END Spanish Analytics Tracking
 });
