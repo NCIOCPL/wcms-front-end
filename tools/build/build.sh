68ff0b09171bc68232b1906a8a2ff98f2f534091
Blair Learn <blairlearn@users.noreply.github.com>
2017-07-11 19:14:02 +0000

    Initial add of build scripts.

---
+++ tools/build/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PROJECT_HOME="$(cd $SCRIPT_PATH/../.. && pwd)"
+
+echo $SCRIPT_PATH
+echo $PROJECT_HOME
+
+pushd CancerGov
+docker build --file Dockerfile/Dockerfile .
+if [ $? != 0 ]; then echo "Docker build failed."; exit(1); fi
+
+pwd
