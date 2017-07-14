3a943131953dd8bb9c048d64172f336dd40ecd13
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-07-14 15:08:45 +0000

    Reverted to R19640 - going back to Trunk state so that we can cleanly merge the latest Trunk back in
    Revision: 19640
    Author: daquinohd
    Date: Monday, July 10, 2017 4:01:07 PM
    Message:
    Creating "pilfering-penguin" branch
    ----
    Added : /content/branches/pilfering-penguin (Copy from path: /content/trunk, Revision, 19639)

--- CancerGov/webpack.config.js
+++ CancerGov/webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
         BasicCTSSearchPage:       './UX/AppModuleSpecific/BasicCTS/Search/BasicCTSSearchPage',
         BasicCTSViewPage:         './UX/AppModuleSpecific/BasicCTS/View/BasicCTSViewPage',
         BasicCTSPrintPage:        './UX/AppModuleSpecific/BasicCTS/Print/BasicCTSPrintPage',
-        BasicCTSAdvSearchPage:    './UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage',
         CTListingPage:            './UX/AppModuleSpecific/BasicCTS/Listing/CTListingPage',
         BlogPostPage:             './UX/PageSpecific/BlogPost/BlogPostPage',
         BlogSeriesPage:           './UX/PageSpecific/BlogSeries/BlogSeriesPage'
@@ -54,7 +53,6 @@ module.exports = {
             BasicCTSSearch: 'UX/AppModuleSpecific/BasicCTS/Search',
             BasicCTSView: 'UX/AppModuleSpecific/BasicCTS/View',
             BasicCTS: 'UX/AppModuleSpecific/BasicCTS',
-            BasicCTSAdvSearch: 'UX/AppModuleSpecific/BasicCTS/AdvSearch',
             Patches: 'Patches',
             Modules: 'Modules',
 
