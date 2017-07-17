36671f8c7377b8ecc64fdc936ae320e8159d8ce8
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-07-17 20:09:21 +0000

    Trying to get imports to work in BasicCTSAdvSearchPage.ts Added .ts file as watched type in watch.js Added compiler options to tsconfig.json

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage.ts
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage.ts
@@ -1,12 +1,15 @@
-import * as $ from "jquery";
+import "Plugins/jquery.nci.equal_heights";
+import * as CTSFormSearchSetup from "UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics"; 
 
+console.log(CTSFormSearchSetup);
 //var $ = require('jquery');
 //require('Plugins/jquery.nci.equal_heights');
 
-$(document).ready(function($){
+$(document).ready(function(){
+	//CTSFormSearchSetup.init();
 	// require('BasicCTSAdvSearch/Enhancements/BasicCTSAdvSearchFormSetup').init();
 	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics').init();
 	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/FeedbackForm').init();
 	// $('[data-match-height]').NCI_equal_heights();
-	console.log('hello world');
-});
+	console.log('hello world 2');   
+});  
