0d84d83ecaadd605ba961006aa7f4aa9679b5f3f
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-07-17 19:01:47 +0000

    - Renamed BasicCTSAdvSearchPage.js to .ts - Added typings to package.json - Added typings.json

---
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage.ts
@@ -0,0 +1,12 @@
+import * as $ from "jquery";
+
+//var $ = require('jquery');
+//require('Plugins/jquery.nci.equal_heights');
+
+$(document).ready(function($){
+	// require('BasicCTSAdvSearch/Enhancements/BasicCTSAdvSearchFormSetup').init();
+	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics').init();
+	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/FeedbackForm').init();
+	// $('[data-match-height]').NCI_equal_heights();
+	console.log('hello world');
+});
