57d9f096df0b1b3349a39f3f53938aec58e0c45e
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-07-17 20:57:46 +0000

    Found workarounds for imports

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage.ts
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/AdvSearch/BasicCTSAdvSearchPage.ts
@@ -1,15 +1,16 @@
 import "Plugins/jquery.nci.equal_heights";
+
 import * as CTSFormSearchSetup from "UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics"; 
 
-console.log(CTSFormSearchSetup);
+
 //var $ = require('jquery');
 //require('Plugins/jquery.nci.equal_heights');
 
 $(document).ready(function(){
 	//CTSFormSearchSetup.init();
+	(<any>(CTSFormSearchSetup)).init();
 	// require('BasicCTSAdvSearch/Enhancements/BasicCTSAdvSearchFormSetup').init();
 	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics').init();
 	// require('UX/AppModuleSpecific/BasicCTS/Common/Enhancements/FeedbackForm').init();
 	// $('[data-match-height]').NCI_equal_heights();
-	console.log('hello world 2');   
 });  
