ed5a53d51d803d5a6df1ee937db73a751851c94d
Blair Learn <blairlearn@users.noreply.github.com>
2017-07-18 16:54:28 +0000

    Make Front-end build a separate operation from building the Dockerfile.

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -7,11 +7,6 @@
 # GITHUB_TOKEN - Github access token for creating releases and uploading build artifacts.
 
 
-# Build arguments are *NOT* recommended as a way to pass secrets (e.g. GITHUB_TOKEN), because
-# they can be revealed by using the `docker history` command to inspect their values.
-# Unfortunately, as Docker doesn't present a mechanism for passing secrets securely, it
-# instead becomes necessary to refrain from sharing the image.
-
 if [ -z "$GH_ORGANIZATION_NAME" ]; then echo GH_ORGANIZATION_NAME not set; exit 1; fi
 if [ -z "$GH_REPO_NAME" ]; then echo GH_REPO_NAME not set; exit 1; fi
 if [ -z "$BUILD_TAG" ]; then echo BUILD_TAG not set; exit 1; fi
@@ -23,12 +18,15 @@ export PROJECT_HOME="$(cd $SCRIPT_PATH/../.. && pwd)"
 pushd CancerGov
 echo Starting build
 #export IMAGE_ID=$(docker build --file Dockerfile/Dockerfile . --quiet)
-docker build --file Dockerfile/Dockerfile . \
-            --build-arg GH_ORGANIZATION_NAME=$GH_ORGANIZATION_NAME \
-            --build-arg GH_REPO_NAME=$GH_REPO_NAME \
-            --build-arg BUILD_TAG=$BUILD_TAG \
-            --build-arg GITHUB_TOKEN=$GITHUB_TOKEN
+docker build --file Dockerfile/Dockerfile --tag wcms-front-end-builder:latest .
 if [ $? != 0 ]; then echo "Docker build failed."; exit 1; fi
 
-# Delete the image. The artifacts are uploaded as part of the build process.
-#docker rmi $IMAGE_ID
+
+docker run --tty --rm  wcms-front-end-builder:latest
+
+# This stuff goes in the script which docker-run executes.
+
+# Clean up old version (if any exists) and create new release.
+#RUN github-release delete --user blairlearn --repo wcms-front-end --tag ${BUILD_TAG} || echo Nothing to delete.
+#RUN github-release release --user ${GH_ORGANIZATION_NAME} --repo ${GH_REPO_NAME} --tag ${BUILD_TAG} --name ${BUILD_TAG}
+
