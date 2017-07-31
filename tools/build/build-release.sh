2ee69e2950fc5d6f1fb85700f0b3b617023d62fb
Blair Learn <blairlearn@users.noreply.github.com>
2017-07-31 16:56:22 -0400

    Configurable release label.
    Add prompt (with default) to let the user specify the text appended at the end of the tag name.
    
    Use this new tag name as the release name as well so multiple release builds can co-exist.  (e.g. Official release build plus an auto-generated one.)

--- tools/build/build-release.sh
+++ tools/build/build-release.sh
@@ -6,13 +6,15 @@
 
 # GH_ORGANIZATION_NAME - The GitHub organization (or username) the repository belongs to. 
 # GH_REPO_NAME - The repository being built.
-# BRANCH_NAME - Name of the branch being built. This will also be used for tagging the build.
 # GITHUB_TOKEN - Github access token for creating releases and uploading build artifacts.
+# BRANCH_NAME - Name of the branch being built. This will also be used for tagging the build.
+# RELEASE_LABEL - label to append to the branch when creating a realeases and tags. 
 
 if [ -z "$GH_ORGANIZATION_NAME" ]; then echo GH_ORGANIZATION_NAME not set; exit 1; fi
 if [ -z "$GH_REPO_NAME" ]; then echo GH_REPO_NAME not set; exit 1; fi
 if [ -z "$BRANCH_NAME" ]; then echo BRANCH_NAME not set; exit 1; fi
 if [ -z "$GITHUB_TOKEN" ]; then echo GITHUB_TOKEN not set; exit 1; fi
+if [ -z "$RELEASE_LABEL"]; then echo RELEASE_LABEL not set; exit 1; fi
 
 npm install
 if [ $? != 0 ]; then echo "npm install failed."; exit 1; fi
@@ -24,9 +26,9 @@ cd _dist
 zip -r wcms-front-end.zip *
 
 # Need the tag name to incorporate, but not be identical to, the branch name.
-export TAG_NAME=${BRANCH_NAME}-latest
+export TAG_NAME=${BRANCH_NAME}-${RELEASE_LABEL}
 
 # Clean up old version (if any exists) and create new release.
 github-release delete --user ${GH_ORGANIZATION_NAME} --repo wcms-front-end --tag ${TAG_NAME} || echo Nothing to delete.
-github-release release --user ${GH_ORGANIZATION_NAME} --repo ${GH_REPO_NAME} --tag ${TAG_NAME} --name ${BRANCH_NAME} --pre-release
+github-release release --user ${GH_ORGANIZATION_NAME} --repo ${GH_REPO_NAME} --tag ${TAG_NAME} --name ${TAG_NAME} --pre-release
 github-release upload --user ${GH_ORGANIZATION_NAME} --repo ${GH_REPO_NAME} --tag ${TAG_NAME} --name built-files.zip --file wcms-front-end.zip
--- tools/build/build.sh
+++ tools/build/build.sh
@@ -3,14 +3,16 @@
 # Required Enviroment Variables
 # GH_ORGANIZATION_NAME - The GitHub organization (or username) the repository belongs to. 
 # GH_REPO_NAME - The repository being built.
-# BRANCH_NAME - Name of the branch being built. This will also be used for tagging the build.
 # GITHUB_TOKEN - Github access token for creating releases and uploading build artifacts.
+# BRANCH_NAME - Name of the branch being built. This will also be used for tagging the build.
+# RELEASE_LABEL - label to append to the branch when creating a realeases and tags. 
 
 
 if [ -z "$GH_ORGANIZATION_NAME" ]; then echo GH_ORGANIZATION_NAME not set; exit 1; fi
 if [ -z "$GH_REPO_NAME" ]; then echo GH_REPO_NAME not set; exit 1; fi
 if [ -z "$BRANCH_NAME" ]; then echo BRANCH_NAME not set; exit 1; fi
 if [ -z "$GITHUB_TOKEN" ]; then echo GITHUB_TOKEN not set; exit 1; fi
+if [ -z "$RELEASE_LABEL"]; then echo RELEASE_LABEL not set; exit 1; fi
 
 export SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PROJECT_HOME="$(cd $SCRIPT_PATH/../.. && pwd)"
@@ -30,6 +32,7 @@ docker run --tty --rm \
     --env GH_REPO_NAME="${GH_REPO_NAME}" \
     --env BRANCH_NAME="${BRANCH_NAME}" \
     --env GITHUB_TOKEN="${GITHUB_TOKEN}" \
+    --env RELEASE_LABEL="${RELEASE_LABEL}" \
     wcms-front-end-builder:$BRANCH_NAME
 if [ $? != 0 ]; then echo "Failed to build ${BRANCH_NAME}."; exit 1; fi
 
