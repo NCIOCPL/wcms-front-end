afee8444705b5f582bd8e363ed7831fcdff2f178
Frank Ali <frank.ali@nih.gov>
2017-08-15 10:51:36 -0400

    [OCECTS-612] Adding polyfills for IE11. Fixing deprecated method on server.js

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Search/BasicCTSSearchPage.ts
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Search/BasicCTSSearchPage.ts
@@ -1,3 +1,7 @@
+//Selective imports in modular pattern
+import 'core-js/fn/object/assign';
+import 'core-js/fn/promise';
+import 'core-js/fn/string/includes';
 import { CTSBasicFormSetup } from 'UX/AppModuleSpecific/BasicCTS/Search/Enhancements/cts-basic-form-setup';
 import { CTSFieldValidator } from 'UX/AppModuleSpecific/BasicCTS/Search/Enhancements/cts-field-validator';
 import * as CTSCommonAnalytics from "UX/AppModuleSpecific/BasicCTS/Common/Enhancements/ctsCommonAnalytics"; 
