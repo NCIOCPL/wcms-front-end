2e3bfe1417a96eae15970358cb93d0f3e6c27094
Dion <daquinohd@users.noreply.github.com>
2017-08-15 18:14:15 -0400

    Added initialize function to toggle enhancement

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/Enhancements/criteriaToggle.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/Enhancements/criteriaToggle.js
@@ -2,27 +2,29 @@
 define(function(require) {
     require('jquery');
 
-    $(".clinicaltrials-results-criteria-display").hide();
-    $('.ctscb').click(function(){
-        var link = $(this);
-        $('.clinicaltrials-results-criteria-display').slideToggle('fast', function() {
-            if ($(this).is(":visible")) {
-                 link.text('Hide Search Criteria');
-            } else {
-                 link.text('Show Search Criteria');
-            }
-        });
+	function _initialize() {
+        $(".clinicaltrials-results-criteria-display").hide();
+        $('.ctscb').click(function(){
+            var link = $(this);
+            $('.clinicaltrials-results-criteria-display').slideToggle('fast', function() {
+                if ($(this).is(":visible")) {
+                    link.text('Hide Search Criteria');
+                } else {
+                    link.text('Show Search Criteria');
+                }
+            });
 
-    });
-    $('.ctscb').toggleClass(function() {
-    $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-minus.png)');
-    //$(".ctscb").toggleClass(
-    //function() {
-    //    $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-minus.png)');
-    //},
-    //function() {
-    //    $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-plus.png)');
-    });
+        });
+        $('.ctscb').toggleClass(function() {
+        $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-minus.png)');
+        //$(".ctscb").toggleClass(
+        //function() {
+        //    $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-minus.png)');
+        //},
+        //function() {
+        //    $(this).css('background-image', 'url(/publishedcontent/images/images/design-elements/icons/cts-plus.png)');
+        });
+    }
 
 /**
  * Identifies if this enhancement has been initialized or not.
