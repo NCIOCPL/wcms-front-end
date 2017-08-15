70acaf00e0d68071c48ba945ace7092d5901cc85
Blair Learn <blairlearn@users.noreply.github.com>
2017-08-15 12:51:51 -0400

    Documentation.

--- CancerGov/_src/Scripts/NCI/Services/cde-configuration-service/model/cde-default-configuration.ts
+++ CancerGov/_src/Scripts/NCI/Services/cde-configuration-service/model/cde-default-configuration.ts
@@ -1,7 +1,13 @@
 import { CDEConfiguration } from './';
 
+/**
+ * Class for specifying default values for runtime configuration options.
+ * 
+ * NOTE: New options must be defined in CDEConfiguration and given explicit default values
+ * in CDEDefaultConfiguration.
+ */
 export class CDEDefaultConfiguration implements CDEConfiguration {
-    // Placeholders all.
+    // These setting names are placeholders
     setting1 : string = 'this is the first default string value';
     setting2 : string = 'this is the second default string value';
     setting3 : number = 87600;
