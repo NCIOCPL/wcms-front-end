915fd195089cbf668590906407151fe168b94172
Blair Learn <blairlearn@users.noreply.github.com>
2017-08-30 14:48:24 -0400

    Turn off npm progress bars

--- CancerGov/tools/build/build-release.sh
+++ CancerGov/tools/build/build-release.sh
@@ -17,6 +17,7 @@ if [ -z "$GITHUB_TOKEN" ]; then echo GITHUB_TOKEN not set; exit 1; fi
 if [ -z "$RELEASE_LABEL" ]; then echo RELEASE_LABEL not set; exit 1; fi
 
 # Move to the CancerGov base directory.
+npm set progress=false
 npm install
 if [ $? != 0 ]; then echo "npm install failed."; exit 1; fi
 
