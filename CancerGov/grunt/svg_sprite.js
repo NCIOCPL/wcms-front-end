7cd48bbfcce67fc1454591e351dd4695938e1f2b
Frank Ali <frank.ali@nih.gov>
2017-09-22 10:52:33 -0400

    Altering sprite calculations (switching from em units to percentages) for Safari.

--- CancerGov/grunt/svg_sprite.js
+++ CancerGov/grunt/svg_sprite.js
@@ -9,13 +9,14 @@ module.exports = function (grunt, options) {
         options: {
             shape: {
                 dimension: {		// Dimension related options
-                    maxWidth: 32,	// Max. shape width
-                    maxHeight: 32,	// Max. shape height
+                    // maxWidth: 32,	// Max. shape width
+                    // maxHeight: 32,	// Max. shape height
                     precision: 5    // Floating point precision
-                },
-                spacing: {
-                    padding: 5      // Padding around all shapes - must remain 5 due to calculation bug in svg-sprite
                 }
+                // ,
+                // spacing: {
+                //     padding: 5      // Padding around all shapes - must remain 5 due to calculation bug in svg-sprite
+                // }
             },
             mode: {
                 css: {       // Activate the «css» mode
@@ -27,7 +28,7 @@ module.exports = function (grunt, options) {
                             template: dirs.src.styles + 'sprites/sprite-template.scss'
                         }
                     },
-                    layout:"pack",
+                    layout:"diagonal",
                     bust: false,
                     example: false
                 }
