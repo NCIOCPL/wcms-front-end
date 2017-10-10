e4d6e420608cdd14eb4443ea2ba7183bcd2af74f
Dion <daquinohd@users.noreply.github.com>
2017-10-10 14:30:20 -0400

    [WCMSPERC-323] removed external link for slick library

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/video-carousel.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/video-carousel.js
@@ -1,9 +1,8 @@
 define(function(require) {
     var $ = require('jquery');
-    var $script = require('scriptjs'); 
+    require('slick-carousel');
     require('Modules/carousel/slick-patch');
     var flexVideo = require('Modules/videoPlayer/flexVideo');
-
 
     /***
     * This snippet uses the slick library to dynamically draw a clickable image carousel based on the playlist ID
@@ -12,67 +11,58 @@ define(function(require) {
     *       make key configurable
     **/
     function _initialize() {
-        $script('//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js',function(){
-            
-            require('Modules/carousel/slick-patch');
-            var flexVideo = require('Modules/videoPlayer/flexVideo');            
+    
+        // Hit the YouTube API and draw the carousel if this is page contains an inline carousel
+        if ($(".yt-carousel")[0]) {
 
-            // Script for carousel
-            $(function() {                   
-    
-                // Hit the YouTube API and draw the carousel if this is page contains an inline carousel
-                if ($(".yt-carousel")[0]) {
-    
-                    // YouTube API address & params
-                    var $playlistId = $(".yt-carousel").attr("data-playlist-id");
-                    var $key = "AIzaSyAc7H6wMKjEqxe2J9iHNnc9OBZhfa6TXN8"; // key for dev work - replace this!!!!
-                    var $maxResults = "50";
-                    var $jsonUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId=" + $playlistId + "&key=" + $key + "&maxResults=" + $maxResults;
+            // YouTube API address & params
+            var $playlistId = $(".yt-carousel").attr("data-playlist-id");
+            var $key = "AIzaSyAc7H6wMKjEqxe2J9iHNnc9OBZhfa6TXN8"; // key for dev work - replace this!!!!
+            var $maxResults = "50";
+            var $jsonUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId=" + $playlistId + "&key=" + $key + "&maxResults=" + $maxResults;
+            
+            // Retrieve video playlist data from the YouTube API. When the object is resolved, build the carousel HTML using each of the playlist's videos.
+            $.getJSON($jsonUrl, {
+                    tagmode: "any",
+                    format: "json"
+                })
+                .done(function(data) {
+                    // Initialize the selected player with the first item in the playlist
+                    var $initialID = (data.items[0].snippet.resourceId.videoId); 
+                    drawSelectedVideo($initialID);                        
                     
-                    // Retrieve video playlist data from the YouTube API. When the object is resolved, build the carousel HTML using each of the playlist's videos.
-                    $.getJSON($jsonUrl, {
-                            tagmode: "any",
-                            format: "json"
-                        })
-                        .done(function(data) {
-                            // Initialize the selected player with the first item in the playlist
-                            var $initialID = (data.items[0].snippet.resourceId.videoId); 
-                            drawSelectedVideo($initialID);                        
-                            
-                            // Draw the carousel thumbnails
-                            // TODO: handle qty of > 50 (API only returns 50 at a time)
-                            $.each(data.items, function(i, item) {
-                                $vid = item.snippet.resourceId.videoId;
-                                $('.yt-carousel-thumbs').append('<a class="yt-carousel-thumb" id="' + $vid + '"><img src="https://i.ytimg.com/vi/' + $vid + '/mqdefault.jpg"></a>');
-                            });
-                            
-                            // JS snippets for YouTube playlist carousel 
-                            // Draw slick slider for YT thumbnails
-                            $('.yt-carousel-thumbs').slick({
-                                infinite: true,
-                                slidesToShow: 3,
-                                slidesToScroll: 1
-                            });
-    
-                            // Script for custom arrows
-                            $('.yt-carousel-arrows .previous').click(function() {
-                                $('.yt-carousel-thumbs').slick("slickPrev");
-                            });
-                            $('.yt-carousel-arrows .next').click(function() {
-                                $('.yt-carousel-thumbs').slick("slickNext");
-                            });
+                    // Draw the carousel thumbnails
+                    // TODO: handle qty of > 50 (API only returns 50 at a time)
+                    $.each(data.items, function(i, item) {
+                        $vid = item.snippet.resourceId.videoId;
+                        $('.yt-carousel-thumbs').append('<a class="yt-carousel-thumb" id="' + $vid + '"><img src="https://i.ytimg.com/vi/' + $vid + '/mqdefault.jpg"></a>');
+                    });
                     
-                            // Change the video on carousel click
-                            $('.yt-carousel-thumb').click(function() {
-                                var $this = $(this);
-                                var $thumbVideoID = $this.attr('id');
-                                drawSelectedVideo($thumbVideoID);
-                            });
-                        });
-                }                
-    
-            });
-        });
+                    // JS snippets for YouTube playlist carousel 
+                    // Draw slick slider for YT thumbnails
+                    $('.yt-carousel-thumbs').slick({
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    });
+
+                    // Script for custom arrows
+                    $('.yt-carousel-arrows .previous').click(function() {
+                        $('.yt-carousel-thumbs').slick("slickPrev");
+                    });
+                    $('.yt-carousel-arrows .next').click(function() {
+                        $('.yt-carousel-thumbs').slick("slickNext");
+                    });
+            
+                    // Change the video on carousel click
+                    $('.yt-carousel-thumb').click(function() {
+                        var $this = $(this);
+                        var $thumbVideoID = $this.attr('id');
+                        drawSelectedVideo($thumbVideoID);
+                    });
+                });
+        }
+
     }
 
     /**
