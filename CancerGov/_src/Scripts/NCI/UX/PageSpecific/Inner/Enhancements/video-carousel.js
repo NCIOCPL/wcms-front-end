63841b28c25003babb70519916a22141470d1154
Dion <daquinohd@users.noreply.github.com>
2017-10-11 14:07:04 -0400

    [WCMSPERC-323] building out >50 items logic

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/video-carousel.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/video-carousel.js
@@ -4,6 +4,9 @@ define(function(require) {
     require('Modules/carousel/slick-patch');
     var flexVideo = require('Modules/videoPlayer/flexVideo');
 
+    // Initialize array of YouTube video IDs
+    var videoList = [];
+    
     /***
     * This snippet uses the slick library to dynamically draw a clickable image carousel based on the playlist ID
     * TODO: - clean up logic for multiple carousels on one page
@@ -26,53 +29,83 @@ define(function(require) {
             $.get($jsonUrl, {
                     part        : 'snippet',
                     playlistId  : $playlistId,
-                    tagmode     : "any",
-                    format      : "json",
-                    key         : $key,
-                    maxResults  : 50
-                })
-                .done(function(data) {
-                    // Initialize the selected player with the first item in the playlist
-                    var $initialID = data.items[0].snippet.resourceId.videoId;
-                    drawSelectedVideo($initialID, $this);
+                    tagmode     : 'any',
+                    format      : 'json',
+                    key         : $key
+                }
+            )
+            .done(function(data) {
+                // Initialize the selected player with the first item in the playlist
+                var $initialID = data.items[0].snippet.resourceId.videoId;
+                drawSelectedVideo($initialID, $this);
 
-                    // Get the number of results
-                    var $count = data.pageInfo.totalResults;
-                    $this.find('.yt-carousel-count').text($count + ' Videos');
-                    
-                    // Draw the carousel thumbnails
-                    // TODO: handle qty of > 50 (API only returns 50 at a time)
-                    $.each(data.items, function(i, item) {
-                        $vid = item.snippet.resourceId.videoId;
-                        $this.find('.yt-carousel-thumbs').append('<a class="yt-carousel-thumb" count="' + i + '" id="' + $vid + '"><img src="https://i.ytimg.com/vi/' + $vid + '/mqdefault.jpg"></a>');
-                    });
-                    
-                    // JS snippets for YouTube playlist carousel 
-                    // Draw slick slider for YT thumbnails
-                    $this.find('.yt-carousel-thumbs').slick({
-                        infinite: true,
-                        slidesToShow: 3,
-                        slidesToScroll: 1
-                    });
+                // Get the number of results
+                var $count = data.pageInfo.totalResults;
+                $this.find('.yt-carousel-count').text($count + ' Videos');
+                
+                setVideoList($jsonUrl, $playlistId, $key);
+                console.log(videoList);
 
-                    // Script for custom arrows
-                    $this.find('.yt-carousel-arrows .previous').click(function() {
-                        $this.find('.yt-carousel-thumbs').slick("slickPrev");
-                    });
-                    $this.find('.yt-carousel-arrows .next').click(function() {
-                        $this.find('.yt-carousel-thumbs').slick("slickNext");
-                    });
-            
-                    // Change the video on carousel click
-                    $this.find('.yt-carousel-thumb').click(function() {
-                        var $th = $(this);
-                        var $thumbVideoID = $th.attr('id');
-                        drawSelectedVideo($thumbVideoID, $this);
-                    });
+                // Draw the carousel thumbnails
+                // TODO: handle qty of > 50 (API only returns 50 at a time)
+                $.each(data.items, function(i, item) {
+                    $vid = item.snippet.resourceId.videoId;
+                    $this.find('.yt-carousel-thumbs').append('<a class="yt-carousel-thumb" count="' + i + '" id="' + $vid + '"><img src="https://i.ytimg.com/vi/' + $vid + '/mqdefault.jpg"></a>');
+                });
+                
+                // JS snippets for YouTube playlist carousel 
+                // Draw slick slider for YT thumbnails
+                $this.find('.yt-carousel-thumbs').slick({
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                });
+
+                // Script for custom arrows
+                $this.find('.yt-carousel-arrows .previous').click(function() {
+                    $this.find('.yt-carousel-thumbs').slick("slickPrev");
+                });
+                $this.find('.yt-carousel-arrows .next').click(function() {
+                    $this.find('.yt-carousel-thumbs').slick("slickNext");
+                });
+        
+                // Change the video on carousel click
+                $this.find('.yt-carousel-thumb').click(function() {
+                    var $th = $(this);
+                    var $thumbVideoID = $th.attr('id');
+                    drawSelectedVideo($thumbVideoID, $this);
                 });
             });
+        });
 
     }
+
+    function setVideoList($url, $plid, $key, $npt)
+    {
+        $.get(
+            $url, {
+            part          : 'snippet',
+            playlistId    : $plid, 
+            tagmode       : 'any',
+            format        : 'json',
+            key           : $key,            
+            pageToken     : $npt, 
+            maxResults    : 50
+            },
+            function(data) { 
+                $.each(data.items, function(i, item) {
+                    $vid = item.snippet.resourceId.videoId;
+                    videoList.push($vid);
+                });
+                if(typeof(data.nextPageToken) !== 'undefined')
+                {
+                    setVideoList($url, $plid, $key, data.nextPageToken)
+                }
+            }
+        );
+    }
+
+    
 
     /**
     * Update flex-video selector attributes with a new video ID
