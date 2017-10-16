6631e80dc3128ab9e4109d176e204c477172e93b
Dion <daquinohd@users.noreply.github.com>
2017-10-16 12:12:13 -0400

    [WCMSPERC-323] building video carousel .ts file

---
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/Inner/Enhancements/video-carousel.ts
@@ -0,0 +1,34 @@
+import { ClinicalTrialsServiceFactory } from 'Services/clinical-trials';
+import { YouTubeAPIFacade } from 'UX/PageSpecific/Inner/Enhancements/youtubeapi-facade';
+import { NCIBaseEnhancement } from 'UX/core';
+
+/**
+ * Class to be used as a base for all CTS forms.
+ * 
+ * @export
+ * @abstract
+ * @class CTSBaseFormSetup
+ * @extends {NCIBaseEnhancement}
+ */
+export class VideoCarousel extends NCIBaseEnhancement{
+		
+
+	/**
+	 * Execute the constructor function on the base enhancement class &
+	 * create an instance of the CT API service.
+	 */
+	constructor() { 
+		super();
+	}
+
+
+	/**
+	 * Initialize this enhancement; Assume it is called from dom ready.
+	 * @return {[type]} Initialize Object
+	 */
+	protected initialize(): void {
+
+        console.log('vc.ts lives');
+	}
+	
+}
