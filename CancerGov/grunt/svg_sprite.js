2c13b70d92b56b51c0e7d86002c3f716ea252b71
Frank Ali <frank.ali@nih.gov>
2017-10-15 21:43:21 -0400

    fixing svg sprite calculations to account for sprite padding properly

--- CancerGov/grunt/svg_sprite.js
+++ CancerGov/grunt/svg_sprite.js
@@ -13,10 +13,10 @@ module.exports = function (grunt, options) {
                     // maxHeight: 32,	// Max. shape height
                     precision: 5    // Floating point precision
                 }
-                // ,
-                // spacing: {
-                //     padding: 5      // Padding around all shapes - must remain 5 due to calculation bug in svg-sprite
-                // }
+                ,
+                spacing: {
+                    padding: 10      // Padding around all shapes - must remain 5 due to calculation bug in svg-sprite
+                }
             },
             mode: {
                 css: {       // Activate the «css» mode
@@ -28,13 +28,16 @@ module.exports = function (grunt, options) {
                             template: dirs.src.styles + 'sprites/sprite-template.scss'
                         }
                     },
-                    layout:"diagonal",
+                    layout:"vertical",
                     bust: false,
                     example: false
                 }
             },
             variables: {
                 mapname: "icons"
+            },
+            svg: {
+	            precision: 5
             }
 
         },
