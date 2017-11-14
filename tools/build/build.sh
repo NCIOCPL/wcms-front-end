2b381da2f1aa2f7cf38711d3b5cc32e11731a186
Blair Learn <blairlearn@users.noreply.github.com>
2017-11-14 15:31:49 -0500

    Verify branch is up to date with master.
    Fail the build immediately if master has commits which aren't in the branch being built.

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -6,6 +6,7 @@
 # GITHUB_TOKEN - Github access token for creating releases and uploading build artifacts.
 # GH_USER - GitHub login of the build user.
 # GH_PASSWORD - Github password for the build user.
+# GIT_COMMIT - sha-1 has of the commit being built.
 # BRANCH_NAME - Name of the branch being built. This will also be used for tagging the build.
 # RELEASE_LABEL - label to append to the branch when creating a realeases and tags. 
 
@@ -15,6 +16,7 @@ if [ -z "$GH_REPO_NAME" ]; then echo GH_REPO_NAME not set; exit 1; fi
 if [ -z "$GITHUB_TOKEN" ]; then echo GITHUB_TOKEN not set; exit 1; fi
 if [ -z "$GH_USER" ]; then echo GH_USER not set; exit 1; fi
 if [ -z "$GH_PASSWORD" ]; then echo GH_PASSWORD not set; exit 1; fi
+if [ -z "$GIT_COMMIT" ]; then echo GH_PASSWORD not set; exit 1; fi
 if [ -z "$BRANCH_NAME" ]; then echo BRANCH_NAME not set; exit 1; fi
 if [ -z "$RELEASE_LABEL" ]; then echo RELEASE_LABEL not set; exit 1; fi
 
@@ -23,7 +25,11 @@ export PROJECT_HOME="$(cd $SCRIPT_PATH/../.. && pwd)"
 
 echo Starting build
 
-# Get the old image ID so we can delete it after a successful build and not
+# Fail the build immediately if master has commits which aren't already in the branch.
+COUNT=`git rev-list --left-only --count master...$GIT_COMMIT`
+if [ $COUNT -ne 0 ]; then echo Error: master is $COUNT commits ahead of $BRANCH_NAME.; exit 1; fi
+
+# Get the old image ID so we can delete it after a successful build and not have
 # a bunch of old images laying around.
 export OLD_IMAGE=$(docker images --quiet wcms-front-end-builder:$BRANCH_NAME)
 
