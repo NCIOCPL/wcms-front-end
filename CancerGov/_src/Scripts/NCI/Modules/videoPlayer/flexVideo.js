31faf86e50849fe6bef1ecd9c218487a9b71fee8
Brendan Beltz <bbeltz@sapient.com>
2017-12-15 14:37:31 -0500

    Initial implementation of thumnail preview hooking

--- CancerGov/_src/Scripts/NCI/Modules/videoPlayer/flexVideo.js
+++ CancerGov/_src/Scripts/NCI/Modules/videoPlayer/flexVideo.js
@@ -59,7 +59,67 @@ define(function(require) {
 						.text(videoText[videoType][lang])
 				);
 			});
-    }
+	}
+
+	const injectIFrame = (videoHook, videoId, videoTitle) => {
+		
+		const pageLang = document.documentElement.lang || 'en'
+		
+		const hl = pageLang === 'es' ? '&hl=es' : '&hl=en'
+		const videoSource = `https://www.youtube-nocookie.com/embed/${videoId}?rel=0${hl}&showinfo=1&autoplay=1`
+		
+		const videoTextTitle = pageLang === 'en' ? 'Youtube embedded video:' : 'Video insertado desde YouTube:'
+		const videoText = `${videoTextTitle} ${videoSource}`
+		
+		const iFrame = document.createElement('iframe')
+		iFrame.setAttribute('frameborder', '0')
+		iFrame.setAttribute('allowFullscreen', '')
+		iFrame.setAttribute('src', videoSource)
+		iFrame.setAttribute('title', videoTitle)
+		iFrame.setAttribute('alt', videoTitle)
+		videoHook.innerHTML = '' // Wipe out Play button and Thumbnail
+		videoHook.appendChild(iFrame)
+
+		const iFrameDocument = iFrame.contentWindow.document
+		const iFrameTitle = iFrameDocument.createTextNode(videoText)
+		iFrameDocument.appendChild(iFrameTitle)
+	}
+	
+	const _newInitialize = () => {
+		const videoHooksNodeList = document.querySelectorAll('.flex-video')
+		const videoHooks = Array.from(videoHooksNodeList)
+		videoHooks.forEach(hook => {
+			// Create preview container
+			const container = document.createElement('div')
+			container.classList.add('video-preview--container')
+
+			// Create Thumbnail Element
+			const thumbnailSource = "https://img.youtube.com/vi/"+ hook.dataset.videoId + "/sddefault.jpg"
+			const thumbnail = new Image()
+			thumbnail.src = thumbnailSource
+			thumbnail.classList.add('video-preview--preview-img')
+			container.appendChild(thumbnail)
+			
+			// Create Play Button Element
+			// This is used for styling the thumbnail to look like a youtube video preclick
+			const playButton = document.createElement('div')
+			playButton.classList.add('video-preview--play-button')
+			container.appendChild(playButton)
+
+			// Create Title Overlay
+			const videoTitle = hook.dataset.videoTitle
+			const titleOverlay = document.createElement('p')
+			const titleText = document.createTextNode(videoTitle)
+			titleOverlay.appendChild(titleText)
+			container.appendChild(titleOverlay)
+
+			// Add to DOM
+			hook.appendChild(container)
+
+			hook.addEventListener('click', () => injectIFrame(hook, hook.dataset.videoId, videoTitle))
+		})
+
+	}
 
     /**
      * Exposed functions of this module.
@@ -67,7 +127,8 @@ define(function(require) {
     return {
         init: function () {
             if (!_initialized) {
-                _initialize();
+				// _initialize();
+				_newInitialize();
             }
         }
     };
