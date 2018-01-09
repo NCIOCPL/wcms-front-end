f70af15ed3c0eabe2332043bb0a2e00a3344f7fc
Mark <markecramer@users.noreply.github.com>
2018-01-09 13:56:38 -0500

    WCMSFEQ-812 - removed JS that controls the feedback from delighter. Also removed the CSS that was put in to hide it.

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/BasicCTSResultsPage.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Results/BasicCTSResultsPage.js
@@ -2,7 +2,6 @@ define(function(require) {
 	var $ = require('jquery');
 
 	$(document).ready(function($){
-		require('BasicCTSCommon/Enhancements/FeedbackForm').init();
 		require('BasicCTSCommon/Enhancements/Delighters').init();
 		require('BasicCTS/Results/Enhancements/Print').init();
 		require('BasicCTSCommon/Enhancements/ctsCommonAnalytics').init();
