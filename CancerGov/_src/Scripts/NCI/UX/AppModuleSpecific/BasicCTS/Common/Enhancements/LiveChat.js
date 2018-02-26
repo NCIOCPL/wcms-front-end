d529d11661ca6bd9d10d8a7419c20e3de48655bc
Frank Ali <frank.ali@nih.gov>
2018-02-26 15:10:01 -0500

    Pointing spanish Live Chat to livehelp-es.cancer.gov

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Common/Enhancements/LiveChat.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/BasicCTS/Common/Enhancements/LiveChat.js
@@ -2,7 +2,9 @@ define(function(require){
 
 	// Which chat server should be used? Test or production.
 	var _hostServer = null; // Will be set in initialize().
+	var _hostServerSpanish = null; // Will be set in initialize().
 	var HOST_SERVER_LIVE = "livehelp.cancer.gov";
+	var HOST_SERVER_LIVE_SPANISH = "livehelp-es.cancer.gov";
 	var HOST_SERVER_TEST = "nci--tst.custhelp.com";
 
 	// Initialization for this enhancement.
@@ -14,16 +16,23 @@ define(function(require){
 		window.open("https://" + _hostServer + "/app/chat/chat_landing?_icf_22=92", "ProactiveLiveHelpForCTS", "height=600,width=633");
 	}
 
+	function _openSpanishChatWindow() {
+		window.open("https://" + _hostServerSpanish + "/app/chat/chat_landing?_icf_22=92", "ProactiveLiveHelpForCTS", "height=600,width=633");
+	}
+
 	// Sets the _hostServer variable to the correct chat server depending on
 	// the current environment.  PROD uses the production server, everywhere
 	// else uses the test server.
 	function _setHostServer() {
-		var currentHost = location.hostname.toLowerCase();
-		
-		if(currentHost === "www.cancer.gov")
-			_hostServer = HOST_SERVER_LIVE;
-		else
+        var currentHost = location.hostname.toLowerCase();
+
+        if (currentHost === "www.cancer.gov") {
+            _hostServer = HOST_SERVER_LIVE;
+			_hostServerSpanish = HOST_SERVER_LIVE_SPANISH;
+		} else {
 			_hostServer = HOST_SERVER_TEST;
+			_hostServerSpanish = HOST_SERVER_TEST;
+		}
 	}
 	
 
@@ -45,7 +54,12 @@ define(function(require){
 		openChatWindow: function(){
 			this.init(); // guarantee init.
 			_openChatWindow();
-		}
+		},
+
+        openSpanishChatWindow: function(){
+            this.init(); // guarantee init.
+            _openSpanishChatWindow();
+        }
 	};
 	
 });
