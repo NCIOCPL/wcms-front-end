bc2dfe136a5aa98cc982bd04fdee54032832373b
mltnewman <mltnewman@users.noreply.github.com>
2018-03-02 12:06:27 -0500

    dropdown & background color

--- CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
+++ CancerGov/_src/Scripts/NCI/UX/PageSpecific/CTHP/CTHPPage.js
@@ -1,7 +1,11 @@
+import dropdownInjector from 'Modules/cthpDropdown';
 import patternInjector from 'Modules/patternInjector';
 import accordionSettings from './accordionSettings';
 import './CTHPPage.scss';
 
-const onDOMContentLoaded = () => patternInjector(accordionSettings);
+const onDOMContentLoaded = () => {
+    dropdownInjector();
+    patternInjector(accordionSettings);
+}
 
 window.addEventListener("DOMContentLoaded", onDOMContentLoaded);
