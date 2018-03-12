2e4afe729f020f546804ff2478eae91eccfbd390
Brendan Beltz <bbeltz@sapient.com>
2018-03-12 16:36:30 -0400

    Capture all sharebuttons on page in object

--- CancerGov/_src/Scripts/NCI/Modules/socialShare/index.js
+++ CancerGov/_src/Scripts/NCI/Modules/socialShare/index.js
@@ -1,8 +1,28 @@
 import { newWindow } from 'Utilities/popups';
 import { getNodeArray, getMetaData } from 'Utilities/domManipulation';
 
-const shareButtonHooks = ['facebook', 'twitter', 'gplus', 'pinterest'];
-const facebookLink = (url) => `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+const FACEBOOK = 'facebook';
+const TWITTER = 'twitter';
+const GOOGLEPLUS = 'googleplus';
+const PINTEREST = 'pinterest';
+const shareButtonHooks = [
+    {
+        type: FACEBOOK,
+        hook: '.social-share--facebook a',
+    },
+    {
+        type: TWITTER,
+        hook: '.social-share--twitter a',
+    },
+    {
+        type: GOOGLEPLUS,
+        hook: '.social-share--googleplus a',
+    },
+    {
+        type: PINTEREST,
+        hook: '.social-share--pinterest a',
+    },
+];
 const metaTags = [
     ['property', 'og:url'],
     ['property', 'og:title'], 
@@ -19,13 +39,28 @@ const getURL = (document, metaData) => {
     return metaURL ? metaURL : getCanonicalURL(document);
 }
 
+const getShareButtonNodes = (shareButtonHooks) => {
+    const shareButtons = shareButtonHooks.map(({type, hook}) => {
+        return {
+            type,
+            hook,
+            nodes: getNodeArray(hook)
+        };
+    });
+    return shareButtons;
+}
+
+const facebookLink = (url) => `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+
 const initialize = () => {
     console.log('Begin SOCIAL SHARE Plugin');
     const metaData = getMetaData(metaTags, document);
     const url = getURL(document, metaData);
+    const shareButtons = getShareButtonNodes(shareButtonHooks);
     console.log({metaData});
     console.log({url});
-    // newWindow(facebookLink(url))
+    console.log({shareButtons})
+    
 }
 
 export default initialize;
