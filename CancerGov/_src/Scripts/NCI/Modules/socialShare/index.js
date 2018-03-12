2e2c0c73360be030a7aa3da1467a9f38a6fee9cc
Brendan Beltz <bbeltz@sapient.com>
2018-03-12 13:30:59 -0400

    Ability to get URL added using canonical for backup

--- CancerGov/_src/Scripts/NCI/Modules/socialShare/index.js
+++ CancerGov/_src/Scripts/NCI/Modules/socialShare/index.js
@@ -1,5 +1,18 @@
+import { newWindow } from 'Utilities/popups';
+import { getNodeArray } from 'Utilities/domManipulation';
+
+const shareButtonHooks = ['facebook', 'twitter', 'gplus', 'pinterest'];
+
+const getCanonicalURL = () => document.querySelector("link[rel='canonical']").href;
+const getMetaURL = () => document.querySelector("meta[property='og:url']").getAttribute('content');
+const getURL = () => {
+    const metaURL = getMetaURL();
+    return metaURL ? metaURL : getCanonicalURL();
+}
+
 const initialize = () => {
-    console.log('Filler for Social Share');
+    console.log('Begin SOCIAL SHARE Plugin');
+    console.log(getURL())
 }
 
 export default initialize;
