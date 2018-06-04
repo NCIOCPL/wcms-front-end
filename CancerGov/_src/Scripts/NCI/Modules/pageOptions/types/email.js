ba8a3bfabea6489a98223a8e2c21e2fcb56e68fa
Brendan Beltz <bbeltz@sapient.com>
2018-06-04 14:02:32 -0400

    Test approach for i18n of content

--- CancerGov/_src/Scripts/NCI/Modules/pageOptions/types/email.js
+++ CancerGov/_src/Scripts/NCI/Modules/pageOptions/types/email.js
@@ -2,15 +2,31 @@ import {
     getMetaData,
     getURL,
     onClickAnalytics,
+    getContent,
 } from '../utilities';
+import {
+    getDocumentLanguage,
+} from 'Utilities/domManipulation';
 
 const email = {
     hook: '.page-options--email a',
-    initialize: () => node => {
+    textContent: {
+        title: {
+            'en': () => 'Email',
+            'es': () => 'Enviar por correo electrónico',
+        },
+        href: {
+            'en': url => `mailto:?subject=Information from the National Cancer Institute Web Site&body=I found this information on www.cancer.gov and I'd like to share it with you: ${ url } \n\n NCI's Web site, www.cancer.gov, contains comprehensive information about cancer causes and prevention, screening and diagnosis, treatment and survivorship; clinical trials; statistics; funding, training and employment opportunities; and the institute and its programs. You can also get cancer information online through the LiveHelp instant messaging service at http://livehelp.cancer.gov. If you live in the United States, you may call the NCI's Cancer Information Service toll-free at 1-800-4-CANCER (1-800-422-6237) for cancer information in English and Spanish.`,
+            'es': url => `mailto:?subject=Donde Esta La Bibliotheca&body=I found this information on www.cancer.gov and I'd like to share it with you: ${ url } \n\n La arana discoteca`,
+        }
+    },
+    initialize: settings => node => {
+        const lang = getDocumentLanguage(window.document);
+        const title = getContent(settings.textContent.title, lang)();
+        node.title = title;
         node.addEventListener('click', () => {
             const url = getURL(document);
-            // TODO: This should be language specific
-            const href = `mailto:?subject=Information from the National Cancer Institute Web Site&body=I found this information on www.cancer.gov and I'd like to share it with you: ${ url } \n\n NCI's Web site, www.cancer.gov, contains comprehensive information about cancer causes and prevention, screening and diagnosis, treatment and survivorship; clinical trials; statistics; funding, training and employment opportunities; and the institute and its programs. You can also get cancer information online through the LiveHelp instant messaging service at http://livehelp.cancer.gov. If you live in the United States, you may call the NCI's Cancer Information Service toll-free at 1-800-4-CANCER (1-800-422-6237) for cancer information in English and Spanish.`;
+            const href = getContent(settings.textContent.href, lang)(url);
             const encodedHref = encodeURI(href);
             node.href = encodedHref;
             node.click();
