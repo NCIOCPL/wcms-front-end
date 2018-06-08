03c2b0e1d9e1ac2674848d879966b811a052cc2b
daquinohd <daquinohd@mail.nih.gov>
2018-06-08 11:08:23 -0400

    Set test eVar

--- CancerGov/_src/Scripts/NCI/analytics/dtm-cust-pagecode.js
+++ CancerGov/_src/Scripts/NCI/analytics/dtm-cust-pagecode.js
@@ -1,5 +1,5 @@
 var waData = document.getElementById('wa-data-element');
 s.channel = waData.dataset.channel;
-s.evar70 = 'sandbox';
+s.eVar70 = 'sandbox';
 s.events = waData.dataset.events;
 // TODO: add logic to set events, props, evars
