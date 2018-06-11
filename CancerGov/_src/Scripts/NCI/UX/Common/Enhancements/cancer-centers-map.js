9fa96f8e393ef13449033d0719f0bec9c1c3c52e
Brendan Beltz <bbeltz@sapient.com>
2018-06-11 08:46:23 -0400

    Remove string literals (don't transpile in require module)

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/cancer-centers-map.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/cancer-centers-map.js
@@ -269,147 +269,147 @@ define(function (require) {
     };
 
     var listingToggle = function () {
-      const AtoZlist_State = `
-      <div id="AtoZlistState" class="az-list">
-        <ul>
-          <li>
-            <a href="#Alabama">A</a>
-          </li>
-          <li>B</li>
-          <li>
-            <a href="#California">C</a>
-          </li>
-          <li>
-            <a href="#District-of-Columbia">D</a>
-          </li>
-          <li>E</li>
-          <li>
-            <a href="#Florida">F</a>
-          </li>
-          <li>
-            <a href="#Georgia">G</a>
-          </li>
-          <li>
-            <a href="#Hawaii">H</a>
-          </li>
-          <li>
-            <a href="#Illinois">I</a>
-          </li>
-          <li>J</li>
-          <li>
-            <a href="#Kansas">K</a>
-          </li>
-          <li>L</li>
-          <li>
-            <a href="#Maine">M</a>
-          </li>
-          <li>
-            <a href="#Nebraska">N</a>
-          </li>
-          <li>
-            <a href="#Ohio">O</a>
-          </li>
-          <li>
-            <a href="#Pennsylvania">P</a>
-          </li>
-          <li>Q</li>
-          <li>R</li>
-          <li>
-            <a href="#South-Carolina">S</a>
-          </li>
-          <li>
-            <a href="#Tennessee">T</a>
-          </li>
-          <li>
-            <a href="#Utah">U</a>
-          </li>
-          <li>
-            <a href="#Virginia">V</a>
-          </li>
-          <li>
-            <a href="#Washington">W</a>
-          </li>
-          <li>X</li>
-          <li>Y</li>
-          <li id="AtoZlistLast">Z</li>
-        </ul>
-      </div>`;
+      const AtoZlist_State = 
+      '<div id="AtoZlistState" class="az-list">' +
+        '<ul>' +
+          '<li>' +
+            '<a href="#Alabama">A</a>' +
+          '</li>' +
+          '<li>B</li>' +
+          '<li>' +
+            '<a href="#California">C</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#District-of-Columbia">D</a>' +
+          '</li>' +
+         '<li>E</li>' +
+          '<li>' +
+            '<a href="#Florida">F</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Georgia">G</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Hawaii">H</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Illinois">I</a>' +
+          '</li>' +
+          '<li>J</li>' +
+          '<li>' +
+            '<a href="#Kansas">K</a>' +
+          '</li>' +
+          '<li>L</li>' +
+          '<li>' +
+            '<a href="#Maine">M</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Nebraska">N</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Ohio">O</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Pennsylvania">P</a>' +
+          '</li>' +
+          '<li>Q</li>' +
+          '<li>R</li>' +
+          '<li>' +
+            '<a href="#South-Carolina">S</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Tennessee">T</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Utah">U</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Virginia">V</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#Washington">W</a>' +
+          '</li>' +
+          '<li>X</li>' +
+          '<li>Y</li>' +
+          '<li id="AtoZlistLast">Z</li>' +
+        '</ul>' +
+      '</div>';
 
-      const AtoZlist_Name = `
-      <div id="AtoZlistName" class="az-list">
-        <ul>
-          <li>
-            <a href="#A">A</a>
-          </li>
-          <li>
-            <a href="#B">B</a>
-          </li>
-          <li>
-            <a href="#C">C</a>
-          </li>
-          <li>
-            <a href="#D">D</a>
-          </li>
-          <li>E</li>
-          <li>
-            <a href="#F">F</a>
-          </li>
-          <li>
-            <a href="#G">G</a>
-          </li>
-          <li>
-            <a href="#H">H</a>
-          </li>
-          <li>
-            <a href="#I">I</a>
-          </li>
-          <li>
-            <a href="#J">J</a>
-          </li>
-          <li>
-            <a href="#K">K</a>
-          </li>
-          <li>
-            <a href="#L">L</a>
-          </li>
-          <li>
-            <a href="#M">M</a>
-          </li>
-          <li>
-            <a href="#N">N</a>
-          </li>
-          <li>
-            <a href="#O">O</a>
-          </li>
-          <li>
-            <a href="#P">P</a>
-          </li>
-          <li>Q</li>
-          <li>
-            <a href="#R">R</a>
-          </li>
-          <li>
-            <a href="#S">S</a>
-          </li>
-          <li>
-            <a href="#T">T</a>
-          </li>
-          <li>
-            <a href="#U">U</a>
-          </li>
-          <li>
-            <a href="#V">V</a>
-          </li>
-          <li>
-            <a href="#W">W</a>
-          </li>
-          <li>X</li>
-          <li>
-            <a href="#Y">Y</a>
-          </li>
-          <li>Z</li>
-        </ul>
-      </div>`;
+      const AtoZlist_Name = 
+      '<div id="AtoZlistName" class="az-list">' +
+        '<ul>' +
+          '<li>' +
+            '<a href="#A">A</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#B">B</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#C">C</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#D">D</a>' +
+          '</li>' +
+          '<li>E</li>' +
+          '<li>' +
+            '<a href="#F">F</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#G">G</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#H">H</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#I">I</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#J">J</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#K">K</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#L">L</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#M">M</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#N">N</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#O">O</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#P">P</a>' +
+          '</li>' +
+          '<li>Q</li>' +
+          '<li>' +
+            '<a href="#R">R</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#S">S</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#T">T</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#U">U</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#V">V</a>' +
+          '</li>' +
+          '<li>' +
+            '<a href="#W">W</a>' +
+          '</li>' +
+          '<li>X</li>' +
+          '<li>' +
+            '<a href="#Y">Y</a>' +
+          '</li>' +
+          '<li>Z</li>' +
+        '</ul>' +
+      '</div>';
       $(document).ready(function () {
         const regionListing = $("#institution-listing1");
         const stateListing = $("#institution-listing2");
