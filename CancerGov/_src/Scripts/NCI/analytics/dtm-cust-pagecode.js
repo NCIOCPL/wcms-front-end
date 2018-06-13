732f61c02c63d50c9420f0035c2e1d37210850fc
daquinohd <daquinohd@mail.nih.gov>
2018-06-13 11:54:57 -0400

    fixed prop/eVar logic

--- CancerGov/_src/Scripts/NCI/analytics/dtm-cust-pagecode.js
+++ CancerGov/_src/Scripts/NCI/analytics/dtm-cust-pagecode.js
@@ -11,24 +11,18 @@
 // *** Original DTM test snippet ***// 
 // Get our custom s object from the analytics data element
 waData = waData || document.getElementById('wa-data-element');
-var MAX_CUSTOMVAR_LEN = 100;
 
 // Set s object pageload values and global variables based on the data element
 s.channel = waData.dataset.channel;
 s.events = waData.dataset.events;
 s.prop10 = document.title;
 
-// TODO: set other params
-// TODO: build our array out of available events, not a fixed length
-// Dynamically add eVars and values to the 's' object
-for(var i = 1; i <= MAX_CUSTOMVAR_LEN; i++) {
-	if(waData.dataset['prop' + i]) {
-		s['prop' + i] = waData.dataset['prop' + i].replace(/(^'+|'+$)/mg, '');
-	}
-}
-
-for(var i = 1; i <= MAX_CUSTOMVAR_LEN; i++) {
-	if(waData.dataset['evar' + i]) {
-		s['eVar' + i] = waData.dataset['evar' + i].replace(/(^'+|'+$)/mg, '');
+// Dynamically props/eVars and values to the 's' object
+for(dataAttr in waData.dataset) {
+	if(dataAttr.includes('prop') || dataAttr.includes('evar'))
+	{
+		var pevKey = dataAttr.replace('v', 'V'); 
+		var pevValue = waData.dataset[dataAttr].replace(/(^'+|'+$)/mg, '');
+		s[pevKey] = pevValue;
 	}
 }
