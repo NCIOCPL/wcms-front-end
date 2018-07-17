d6c3b7c07aca1e0232ebeefa09fb156d14682922
BrendanBeltzNIH <34315527+BrendanBeltzNIH@users.noreply.github.com>
2018-07-17 12:06:17 -0400

    [WCMSFEQ-1040] Remove Modernizr dependency (#198)
    * Remove unused modernizr dependency
    
    * Remove redundant CSS selectors using modernizr
    
    * Delete modernizrrc file
    
    * Add release notes

--- CancerGov/webpack.config.js
+++ CancerGov/webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
 	// TODO: Add CDN paths
 	entry: {
 		// Vendor:                   [
-		//                             'modernizr'
 		//                             // 'jquery-ui/ui/widgets/accordion',
 		//                             // 'jquery-ui/ui/widgets/autocomplete',
 		//                             // 'jquery-ui/ui/widgets/dialog',
@@ -27,7 +26,7 @@ module.exports = {
 		//                           ],
 		//This is the Babel polyfill module that includes all the es2015 polyfills.
 		//"Babel-Polyfill":       'babel-polyfill',
-		Common:             ['modernizr','./UX/Common/Common'],
+		Common:             	'./UX/Common/Common',
 		CTHPPage:               './UX/PageSpecific/CTHP/CTHPPage',
 		HomePage:               './UX/PageSpecific/Home/HomePage',
 		InnerPage:              './UX/PageSpecific/Inner/InnerPage',
@@ -71,7 +70,6 @@ module.exports = {
 			// vendor scripts
 			// jquery$: '//code.jquery.com/jquery-3.1.1.min.js',
 			// 'jquery-ui': '//code.jquery.com/ui/1.12.1/jquery-ui.min.js',
-			modernizr$: path.resolve(__dirname, "./.modernizrrc"),
 			Headroom$: 'headroom.js/dist/headroom.min',
 
 			// vendor jQuery plugins
@@ -119,7 +117,6 @@ module.exports = {
 			// this transpiles the TS to ES2015 Javascri[t, which is then handed off to Babel
 			{ test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 			{ test: /\.h(andle)?b(ar)?s$/i, loader: "handlebars-loader" },
-			{ test: /\.modernizrrc$/, loader: "expose-loader?Modernizr!modernizr-loader!json-loader" },
 			{ 
 				test: /\.js$/,
 				exclude: /(node_modules|bower_components)/,
@@ -150,7 +147,6 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.ProvidePlugin({
-			Modernizr: "modernizr",
 			Chart: 'Charts',
 			Headroom: 'Headroom'
 		}),
