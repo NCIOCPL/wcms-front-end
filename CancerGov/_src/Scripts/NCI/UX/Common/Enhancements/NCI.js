ee8dc1c6a3ed5941339f996966c74a14d049a2b9
Frank Ali <frank.ali@nih.gov>
2018-08-14 08:26:54 -0400

    Removing NCI.dictionary and NCI.doAutocomplete from NCI global object

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/NCI.js
@@ -1,10 +1,8 @@
 define(function (require) {
 	var NCI = {
 
-		dictionary: require('Data/DictionaryService'),
-
 		// doAutocomplete is used in backend JavaScript placed on the dictionary terms page "/publications/dictionaries/cancer-terms"
-		doAutocomplete: require('Modules/autocomplete/autocomplete').doAutocomplete,
+		// doAutocomplete: require('Modules/autocomplete/autocomplete').doAutocomplete,
 
 		window: {}
 	};
