dd5750e726cd7d91b7cc9111e5c7de3061915ab7
Frank Ali <frank.ali@nih.gov>
2018-10-21 10:23:23 -0400

    replacing scrolltofixed plugin with scrollMonitor and native events

--- CancerGov/_src/Scripts/NCI/Modules/headroom/headroom.js
+++ CancerGov/_src/Scripts/NCI/Modules/headroom/headroom.js
@@ -1,17 +1,53 @@
 import './headroom-patch';
 // we still need the jQuery wrapper
 import 'headroom.js/dist/jQuery.headroom.min';
-import 'jquery/scrollToFixed';
+import scrollMonitor from "scrollmonitor";
+import { debounce } from 'throttle-debounce';
 
 function _initialize() {
 	/*** BEGIN Headroom initializer
 	 * (use this if we do the scroll off/on for the blue bar)
 	 ***/
 
-	// initialize scrollToFixed plugin
-	$('.fixedtotop').scrollToFixed({
-		spacerClass: 'fixedtotop-spacer',
-		dontSetWidth: true
+	const banner = document.getElementById("nvcgSlSiteBanner");
+	const header = banner.parentElement;
+
+	// set up scroll monitor listener on the header - this will trigger the fixed and unfixed positioning of the main navigation as the header enters and exits the viewport
+	const headerMonitor = scrollMonitor.create(header);
+	const menu = document.querySelector(".fixedtotop");
+
+	const handleScrollIn = () => {
+		requestAnimationFrame(function() {
+			menu.setAttribute("style", `position:relative`);
+			header.classList.add('noMargin');
+		});
+	}
+	const handleScrollOut = () => {
+		requestAnimationFrame(function() {
+			menu.setAttribute("style", `position:fixed`);
+			header.classList.remove('noMargin');
+		});
+	}
+
+	const debouncedResize = debounce(100,function(){
+		if(menu.style.position === 'fixed'){
+			handleScrollOut();
+		}
+	});
+
+	headerMonitor.exitViewport(() => {
+		handleScrollOut();
+		window.removeEventListener('scroll',handleScrollIn);
+	});
+
+	headerMonitor.enterViewport(() => {
+		handleScrollIn();
+		window.addEventListener('scroll',handleScrollIn);
+	});
+
+	window.addEventListener('resize', debouncedResize, {
+		capture: true,
+		passive: true
 	});
 
 	$('.headroom-area').headroom({
