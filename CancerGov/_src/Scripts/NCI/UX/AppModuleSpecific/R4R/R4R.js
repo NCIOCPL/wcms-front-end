92d3f6e1b9fed95ee4c11eb293b71a20a1960d59
Sarina <sarinapadilla@users.noreply.github.com>
2018-10-24 16:37:59 -0400

    WCMSFEQ-1183: Adding redirect for specific URLs.
    - Need to do this the hacky way in here, since the redirect map doesn't work with push state.

--- CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/R4R/R4R.js
+++ CancerGov/_src/Scripts/NCI/UX/AppModuleSpecific/R4R/R4R.js
@@ -2,6 +2,71 @@ import initializeR4R from 'r4r-app';
 import 'r4r-app/build/static/css/main.css';
 import initializeCancerGov from './config';
 
+const homeRedirects = ["/research/resources/biospecimens-video",  "/research/resources/biospecimens-video/",
+                "/research/resources/conducting", "/research/resources/conducting/", 
+                "/research/resources/data-catalog", "/research/resources/data-catalog/",
+                "/research/resources/statistical-tools", "/research/resources/statistical-tools/",
+                "/research/resources/terminology", "/research/resources/terminology/",
+                "/research/resources/terminology/fmt", "/research/resources/terminology/fmt/",
+                "/research/resources/terminology/ncidictionaries", "/research/resources/terminology/ncidictionaries/"];
+
+const path = window.location.pathname;
+
+function needsRedirect() {
+    let redirect = false;
+
+    if(homeRedirects.indexOf(path) > -1)
+    {
+        redirect = redirectUrl("/research/resources");
+    }
+
+    switch (path) {
+        // Resource 230 is CDISC Terminology
+        case "/research/resources/terminology/cdisc":
+            redirect = redirectUrl("/research/resources/resource/230");
+            break;
+        case "/research/resources/terminology/cdisc/":
+            redirect = redirectUrl("/research/resources/resource/230");
+            break;
+    
+        // Resource 196 is FDA Terminology
+        case "/research/resources/terminology/fda":
+            redirect = redirectUrl("/research/resources/resource/196");
+            break;
+        case "/research/resources/terminology/fda/":
+            redirect = redirectUrl("/research/resources/resource/196");
+            break;
+        
+        // Resource 236 is NCPDP Terminology
+        case "/research/resources/terminology/ncpdp":
+            redirect = redirectUrl("/research/resources/resource/236");
+            break;
+        case "/research/resources/terminology/ncpdp/":
+            redirect = redirectUrl("/research/resources/resource/236");
+            break;
+    
+        // Resource 237 is Pediatric Terminology
+        case "/research/resources/terminology/pediatric":
+            redirect = redirectUrl("/research/resources/resource/237");
+            break;
+        case "/research/resources/terminology/pediatric/":
+            redirect = redirectUrl("/research/resources/resource/237");
+            break;
+
+        default:
+            break;
+    }
+
+    return redirect;
+}
+
+function redirectUrl(url)
+{
+    console.log("In redirectUrl() with " + url);
+    window.location.replace(url);
+    return true;
+}
+
 const onDOMContentLoaded = () => {
     // React Helmet is removing all elements with data-react-helmet="true" on the page except the ones it directly controls. This is a big
     // issue. This code adds it only to the elements we want to control manually to avoid that issue.
@@ -24,4 +89,6 @@ const onDOMContentLoaded = () => {
     initializeCancerGov(initializeR4R);
 }
 
-document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+if (!needsRedirect()) {
+    document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+}
