6752642332045c70241878aacd35efa9bd07c677
daquinohd <daquinohd@mail.nih.gov>
2018-10-24 12:51:07 -0400

    Whitespace fix

--- CancerGov/grunt/uglify.js
+++ CancerGov/grunt/uglify.js
@@ -10,14 +10,14 @@ module.exports = function (grunt, options) {
         src: [dirs.src.scripts + 'NCI_OLD/*.js']
     };
     var analyticsJsFiles = {
-	    expand: true,
-	    flatten: true,
-	    dest: dirs.dist.scripts + 'analytics',
-	    src: [dirs.src.scripts + 'NCI/analytics/AppMeasurement*.js']
+        expand: true,
+        flatten: true,
+        dest: dirs.dist.scripts + 'analytics',
+        src: [dirs.src.scripts + 'NCI/analytics/AppMeasurement*.js']
     };
     var analyticsHtmlFiles = {
-	    expand: true,
-	    flatten: true,
+        expand: true,
+        flatten: true,
         dest: dirs.dist.files + 'analytics',
         src: [dirs.src.scripts + 'NCI/analytics/!(AppMeasurement)*.js'],
         ext: '.inc'
@@ -59,31 +59,29 @@ module.exports = function (grunt, options) {
                 src: [dirs.src.base + 'Widgets/**/*.js']                
             }]
         },
-	    analyticsJs: {
-		    options: {
-		        compress: {
-			        collapse_vars: false,
-			        keep_fnames: true,
-			        unused: false,
-
+        analyticsJs: {
+            options: {
+                compress: {
+                    collapse_vars: false,
+                    keep_fnames: true,
+                    unused: false,
                 },
                 mangle: true
-		    },
-		    files: [analyticsJsFiles]
+            },
+            files: [analyticsJsFiles]
         },
         analyticsHtml: {
-		    options: {
-		        compress: {
-			        collapse_vars: false,
-			        keep_fnames: true,
-			        unused: false,
-
+            options: {
+                compress: {
+                    collapse_vars: false,
+                    keep_fnames: true,
+                    unused: false,
                 },
                 mangle: true,
                 banner: '<script>',
                 footer: '</script>'
-		    },
-		    files: [analyticsHtmlFiles]
+            },
+            files: [analyticsHtmlFiles]
         }
         
     }
