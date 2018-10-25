033d33810ca18ff70d6412aaa20034303dc86859
daquinohd <daquinohd@mail.nih.gov>
2018-10-25 09:51:08 -0400

    Changed built file destination / filetype

--- CancerGov/grunt/uglify.js
+++ CancerGov/grunt/uglify.js
@@ -18,9 +18,9 @@ module.exports = function (grunt, options) {
     var analyticsHtmlFiles = {
         expand: true,
         flatten: true,
-        dest: dirs.dist.files + 'analytics',
+        dest: dirs.dist.scripts + 'analytics',
         src: [dirs.src.scripts + 'NCI/analytics/!(AppMeasurement)*.js'],
-        ext: '.inc'
+        ext: '.html'
     };
 
     return {
@@ -44,19 +44,19 @@ module.exports = function (grunt, options) {
         },
         dictionaryWidget: {
             options: {
-		        compress: {
-			        collapse_vars: false,
-			        keep_fnames: true,
-			        unused: false,
+                compress: {
+                    collapse_vars: false,
+                    keep_fnames: true,
+                    unused: false,
 
                 },
-			    mangle: true
+                mangle: true
             },
             files: [{
                 expand: true,
                 flatten: true,
                 dest: dirs.dist.scripts + 'dictionaryWidget',
-                src: [dirs.src.base + 'Widgets/**/*.js']                
+                src: [dirs.src.base + 'Widgets/**/*.js']
             }]
         },
         analyticsJs: {
