c3c9f25d9ebaf7a095c119f0116f4bc996671791
Frank Ali <frank.ali@nih.gov>
2018-11-28 09:19:15 -0500

    Fisrt pass at making Dictionary Service API calls for modals

--- CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/popup_functions.js
+++ CancerGov/_src/Scripts/NCI/UX/Common/Enhancements/popup_functions.js
@@ -1,6 +1,17 @@
+import $ from 'jquery';
+import dictionary from 'Data/DictionaryService';
+import queryString from 'query-string';
+
+var lang = $('html').attr('lang') || 'en';
+// Set the language for finding the dictionary term/definition
+var longLang = 'English'; 
+if (lang === 'es') {
+	longLang = 'Spanish';
+}
+
 const popupFunctions = () => {
 	//creates appropriate pop-up window
-	function popWindow(type, urlargs){
+	function popWindowOld(type, urlargs){
 		if (type == "privacy") {
 			window.open('/common/popUps/popPrivacy.aspx','','scrollbars=no,resizable=yes,width=300,height=300');
 		} else if (type == "livehelp") {
@@ -18,23 +29,85 @@ const popupFunctions = () => {
 			window.open(urlargs, '', 'scrollbars=no,resizable=no,menubar=no,status=no,toolbar=no,titlebar=no,width=200,height=100,left=400,screenX=400,top=300,screenY=300');
 		}
 	} 
+
+	// get the full definition from the dictionary service
+	var _getDefinition = function (term) {
+		return dictionary.search('term', term, longLang, 'exact');
+	};
+	// get the full definition from the dictionary service
+	var _getTerm = function (id) {
+		return dictionary.getTerm('term', id, longLang);
+	};
+
+	function popWindow(type, urlargs){
+		let url = '';
+		if (type == "privacy") {
+			url = '/common/popUps/popPrivacy.aspx';
+			
+		} else if (type == "livehelp") {
+			url = '/common/popUps/popLiveHelp.aspx';
+			
+		} else if (type == "definition") {
+			let term = urlargs.replace(/\s/g, '+');
+			$.when(_getDefinition(term)).done(function (termObject) {
+				console.log(termObject);
+				// if (termObject.result.length > 0) {
+				// 	console.log(termObject);
+				// 	// _render(termObject.result[0].term);
+				// }
+			});
+			
+		} else if (type == "defbyid") {
+			let params = queryString.parse(urlargs);
+			// id's are prefixed with "CDR0000" in the html but the backend service errors out if included in request
+			let id = Object.keys(params)[0].replace("CDR0000",''); 
+			console.log("params",params);
+
+			$.when(_getTerm(id)).done(function (termObject) {
+				//TODO: error returns 404 html page, not an error object
+				if (termObject.term) {
+					console.log(termObject.term);
+					//TODO: render term to dialog
+					// _render(termObject.result[0].term);
+				}
+			});
+			
+		} else {
+			window.open(urlargs, '', 'scrollbars=yes,resizable=yes,width=550,height=550');
+		}
+
+		//console.log(params);
+
+
+
+		// currently on the href
+		//popWindow('defbyid','CDR0000045333&version=patient&language=English&dictionary=Cancer.gov'); - this would be a getTerm request
+		//popWindow('definition','malignant') - this would be an search request with an exact match option
+
+		// using the dictionary service - from bestbets.js
+		//https://www.cancer.gov/Dictionary.Service/v1/search?dictionary=term&searchText=prostate&language=English&searchType=exact&offset=0&maxResuts=0
+
+		//  search: function(dictionary, searchText, language, searchType, offset, maxResults) {
+		// getTerm: function(dictionary, termID, language) {
+	}
+
 	window.popWindow = popWindow;
 
 	function dynPopWindow(url, name, windowAttributes){
-		options = '';
-		optWidth = 'width=500';
-		optHeight = 'height=500';
-		optScrollbar = 'scrollbars=yes';
-		optResizable = 'resizable=yes';
-		optMenubar = 'menubar=yes';
-		optLocation = 'location=yes';
-		optStatus = 'status=yes';
-		optToolbar = 'toolbar=yes';
+		var options = '';
+		var optWidth = 'width=500';
+		var optHeight = 'height=500';
+		var optScrollbar = 'scrollbars=yes';
+		var optResizable = 'resizable=yes';
+		var optMenubar = 'menubar=yes';
+		var optLocation = 'location=yes';
+		var optStatus = 'status=yes';
+		var optToolbar = 'toolbar=yes';
 
-		windowOptions = windowAttributes.split(',');
+		var windowOptions = windowAttributes.split(',');
 
 		for(i = 0; i < windowOptions.length; i++){
-			attribute = windowOptions[i].substring(0, windowOptions[i].indexOf('=')).toLowerCase();
+			var attribute = windowOptions[i].substring(0, windowOptions[i].indexOf('=')).toLowerCase();
 
 			if(attribute == 'width'){
 				optWidth = windowOptions[i];
