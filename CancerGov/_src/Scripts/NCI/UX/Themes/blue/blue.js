bfff3b72f18992dbb674e91d5daf4df1db9ba4c1
Mark Cramer <cramerm@mail.nih.gov>
2019-01-22 18:37:17 -0500

    Image color changes, stylesheet menu twerks, added JS that was located in a content block to the JS file.

--- CancerGov/_src/Scripts/NCI/UX/Themes/blue/blue.js
+++ CancerGov/_src/Scripts/NCI/UX/Themes/blue/blue.js
@@ -2,6 +2,22 @@ console.log("This file should only be loaded for NCI Connect");
 import './blue.scss';
 
 const onDOMContentLoaded = () => {
-  console.log("DOMContentLoaded triggered");
+  var micrositeSnippet = function (i) {
+    function e() {
+      var i = $("#microsite-a");
+      i.length > 0 && ($("#nvcgSlUtilityBar").addClass("micro-a"), $(".row.guide-card").addClass("micro-a"), $(".feature-primary-title h3, .guide-title h2").each(function (i) {
+        $this = $(this), $this.html().trim() || $this.remove()
+      }), "/" == $(".breadcrumbs li a").first().attr("href") && $(".breadcrumbs li").first().remove())
+    }
+    t = !1;
+    return {
+      init: function () {
+        t || (e(), t = !0);
+      }
+    }
+  }
+  $(document).ready(function () {
+    micrositeSnippet().init();
+  }); // Remove the language bar and persist the utility bar past mobile. $('.language-bar').first().addClass('hide-for-medium-down'); 
 }
 
