91a10a9632709231c794dd6eeb90be93d70dfc55
markecramer <markecramer@users.noreply.github.com>
2019-04-09 17:54:27 -0400

    Moving JS from Percussion to themes JS file.

--- CancerGov/_src/Scripts/NCI/UX/Themes/blue/blue.js
+++ CancerGov/_src/Scripts/NCI/UX/Themes/blue/blue.js
@@ -1,7 +1,24 @@
 import './blue.scss';
 
 const onDOMContentLoaded = () => {
-  
+var micrositeSnippet = function(i) {
+  function e() {
+    var i = $("#microsite-a");
+    var $this;
+    i.length > 0 && ($("#nvcgSlUtilityBar").addClass("micro-a"), $(".row.guide-card").addClass("micro-a"), $(".feature-primary-title h3, .guide-title h2").each(function(i) {
+      $this = $(this), $this.html().trim() || $this.remove()
+    }), "/" == $(".breadcrumbs li a").first().attr("href") && $(".breadcrumbs li").first().remove())
+  }
+  var t = !1;
+  return {
+    init: function() {
+      t || (e(), t = !0)
+    }
+  }
 }
+$(document).ready(function() {
+  micrositeSnippet().init()
+});
+};
 
 document.addEventListener('DOMContentLoaded',onDOMContentLoaded);
--- CancerGov/_src/Scripts/NCI/UX/Themes/purple/purple.js
+++ CancerGov/_src/Scripts/NCI/UX/Themes/purple/purple.js
@@ -2,7 +2,24 @@ console.log("This file should only be loaded for NCI Connect");
 import './purple.scss';
 
 const onDOMContentLoaded = () => {
-  console.log("DOMContentLoaded triggered");
-}
-
-document.addEventListener('DOMContentLoaded',onDOMContentLoaded);
+  var micrositeSnippet = function(i) {
+    function e() {
+      var i = $("#microsite-a");
+      var $this;
+      i.length > 0 && ($("#nvcgSlUtilityBar").addClass("micro-a"), $(".row.guide-card").addClass("micro-a"), $(".feature-primary-title h3, .guide-title h2").each(function(i) {
+        $this = $(this), $this.html().trim() || $this.remove()
+      }), "/" == $(".breadcrumbs li a").first().attr("href") && $(".breadcrumbs li").first().remove())
+    }
+    var t = !1;
+    return {
+      init: function() {
+        t || (e(), t = !0)
+      }
+    }
+  }
+  $(document).ready(function() {
+    micrositeSnippet().init()
+  });
+  };
+  
+  document.addEventListener('DOMContentLoaded',onDOMContentLoaded);
